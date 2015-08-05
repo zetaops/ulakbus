83a20d5bf5131482b431ef3c610c798be9f0bc84
Evren Esat Özkan <sleytr@gmail.com>
2015-08-06 00:29:07 +0300

    py.test using requests library against wsgiref.simple_server

--- tests/test_form_from_model.py
+++ tests/test_form_from_model.py
@@ -1,6 +1,10 @@
+from wsgiref import simple_server
+from zengine.dispatcher import app
 from tests.deep_eq import deep_eq
 from tests.models import Employee
 from zengine.lib.forms import AngularForm
+from tests.test_utils import Request
+
 __author__ = 'Evren Esat Ozkan'
 
 serialized_empty_test_employee = {
@@ -23,3 +27,20 @@ def test_simple():
     serialized_form = AngularForm(Employee()).serialize()
     # assert serialized_empty_test_employee['model'] == serialized_form['model']
     assert deep_eq(serialized_empty_test_employee, serialized_form,_assert=True )
+
+class TestCase:
+
+    httpd = None
+    @classmethod
+    def prepare_server(self):
+        self.httpd = simple_server.make_server('0.0.0.0', 9001, app)
+
+
+    def test_real_login(self):
+        try:
+            self.prepare_server()
+            client = Request('simple_login')
+            client.request()
+            self.httpd.handle_request()
+        finally:
+            self.httpd.shutdown()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,30 @@
 import os
+import threading
+from time import sleep
 
 __author__ = 'Evren Esat Ozkan'
 
 
 def get_worfklow_path(wf_name):
     return "%s/workflows/%s.zip" % (os.path.dirname(os.path.realpath(__file__)), wf_name)
+
+
+import requests
+from pprint import pprint
+import json
+
+
+class Request(object):
+    def __init__(self, wfname):
+        self.wfname = wfname
+
+    def request(self, **data):
+        threading.Thread(target=self._request, args=(self.wfname,), kwargs=data).start()
+
+    def _request(wfname, data):
+        url = 'http://localhost:9001/%s/' % wfname
+        sleep(0.1)
+        r = requests.post(url, data=json.dumps(data))
+        pprint(r.headers)
+        print(r.content)
+
