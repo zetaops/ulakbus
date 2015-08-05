62e233a96ed2de026f11cdf860d2e0536d51cccd
Evren Esat Özkan <sleytr@gmail.com>
2015-08-05 10:39:01 +0300

    added lazy_object_proxy to requirements (pyoko dependency) modified test_simple to use deep_eq for comparision

--- tests/test_form_from_model.py
+++ tests/test_form_from_model.py
@@ -1,16 +1,15 @@
+from tests.deep_eq import deep_eq
 from tests.models import Employee
 from ulakbus.modules.forms import AngularForm
-
 __author__ = 'Evren Esat Ozkan'
 
 serialized_empty_test_employee = {
-    'model': {'birth_date': '', 'archived': False, 'last_name': None,
+    'model': {'birth_date': '', 'last_name': None,
               'first_name': None, 'staff_type': None},
-    'form': ['archived', 'last_name', 'first_name', 'staff_type', 'birth_date'],
-    'schema': {'required': ['archived', 'last_name', 'first_name', 'staff_type', 'birth_date'],
+    'form': ['first_name', 'last_name', 'staff_type', 'birth_date'],
+    'schema': {'required': ['first_name','staff_type', 'birth_date', 'last_name'],
                'type': 'object',
                'properties': {'birth_date': {'type': 'date', 'title': 'Do\xc4\x9fum Tarihi'},
-                              'archived': {'type': 'boolean', 'title': ''},
                               'last_name': {'type': 'string', 'title': 'Soyad\xc4\xb1'},
                               'first_name': {'type': 'string', 'title': 'Ad\xc4\xb1'},
                               'staff_type': {'type': 'string',
@@ -19,5 +18,8 @@ serialized_empty_test_employee = {
 
 
 def test_simple():
+
+
     serialized_form = AngularForm(Employee()).serialize()
-    assert serialized_empty_test_employee == serialized_form
+    # assert serialized_empty_test_employee['model'] == serialized_form['model']
+    assert deep_eq(serialized_empty_test_employee, serialized_form,_assert=True )
