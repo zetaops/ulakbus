8d03a185b4cb193db80a04914faec549f71a31f4
Evren Esat Özkan <sleytr@gmail.com>
2015-08-10 13:26:06 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     version='0.0.1',
     description='Ulakbus Butunlesik Universite Sistemi',
     author='Zetaops',
-    requires=['beaker', 'falcon', 'beaker_extensions', 'redis', 'SpiffWorkflow', 'zengine'],
-    install_requires=['beaker', 'falcon', 'beaker_extensions', 'redis', 'SpiffWorkflow', 'zengine'],
-    dependency_links=['git+https://github.com/didip/beaker_extensions.git#egg=beaker_extensions',
-                      'git+https://github.com/zetaops/SpiffWorkflow.git#egg=SpiffWorkflow',
-                      'git+https://github.com/zetaops/zengine.git#egg=zengine'],
+    requires=['zengine', 'pyoko'],
+    install_requires=['pyoko', 'zengine'],
+    dependency_links=[
+        'git+https://github.com/zetaops/pyoko.git#egg=pyoko',
+        'git+https://github.com/zetaops/zengine.git#egg=zengine'],
 
     author_email='info@zetaops.io',
     url='https://github.com/zetaops/ulakbus',
