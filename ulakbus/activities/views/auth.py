3711cb266ad54650011065f57c839b79b56afc6d
Evren Esat Özkan <sleytr@gmail.com>
2015-08-16 14:44:28 +0300

    working on authentication and roles

--- ulakbus/activities/views/auth.py
+++ ulakbus/activities/views/auth.py
@@ -32,18 +32,14 @@ class Login(SimpleView):
             raise HTTPBadRequest("Eksik bilgi girdiniz",
                                  "Lütfen kullanıcı adınızı ve parolanızı giriniz")
         try:
-            user = User.objects.filter(username=username).get()
-
-            is_login_successful = user.check_password(password)
-            if is_login_successful:
-                # self.current.output = {'success': True}
-                self.current.session['user_id'] = user.key
-            self.current.task_data['IS'].login_successful = is_login_successful
+            self.current.task_data[
+                'IS'].login_successful = self.current.auth.authenticate(
+                username, password)
 
         except IndexError:
             raise HTTPUnauthorized('Giriş bilgileri hatalı',
-                                   "Girdiğiniz kullanıcı adı ya da parola ile eşleşen bir "
-                                   "kullanıcı kaydı bulamadık")
+                                   "Girdiğiniz kullanıcı adı ya da parola ile "
+                                   "eşleşen bir kullanıcı kaydı bulamadık")
 
     def _show(self):
         self.current.session['dfdf'] = 'sdfdf'
