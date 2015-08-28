5f22d5feaa64efae09dd7319dc8b6b9ad977844f
Evren Esat Özkan <evrenesat@zetaops.io>
2015-08-28 17:22:21 +0300

    refactored the settings.py according to zengine's modifications removed unneeded modules

--- tests/test_cruds.py
+++ tests/test_cruds.py
@@ -6,25 +6,26 @@
 #
 # This file is licensed under the GNU General Public License v3
 # (GPLv3).  See LICENSE.txt for details.
-from time import sleep
+
 from pyoko.model import model_registry
-from tests.test_utils import BaseTestCase
+from base_test_case import BaseTestCase
+
 RESPONSES = {}
+
 
 class TestCase(BaseTestCase):
     def test_list_add_delete_with_employee_model(self):
-
         # setup workflow
         self.prepare_client('crud')
 
         # calling the crud view without any model should list available models
         resp = self.client.post()
-        assert resp.json['models'] == [m.__name__ for m in
-                                       model_registry.get_base_models()]
+        assert resp.json['models'] == [m.__name__ for m in model_registry.get_base_models()]
 
         # calling with just model name (without any cmd) equals to cmd="list"
         resp = self.client.post(model='Employee')
         assert 'objects' in resp.json
+
         list_objects = resp.json['objects']
         if list_objects:
             assert list_objects[0]['data']['first_name'] == 'Em1'
@@ -33,7 +34,7 @@ class TestCase(BaseTestCase):
         num_of_objects = len(resp.json['objects'])
 
         # add a new employee record, then go to list view (do_list subcmd)
-        self.client.post(model='Employee',cmd='add')
+        self.client.post(model='Employee', cmd='add')
         resp = self.client.post(model='Employee',
                                 cmd='add',
                                 subcmd="do_list",
--- ulakbus/settings.py
+++ ulakbus/settings.py
@@ -6,34 +6,41 @@
 # This file is licensed under the GNU General Public License v3
 # (GPLv3).  See LICENSE.txt for details.
 __author__ = 'Evren Esat Ozkan'
-
+from zengine.settings import *
 import os.path
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # path of the activity modules which will be invoked by workflow tasks
-ACTIVITY_MODULES_IMPORT_PATH = 'ulakbus.activities'
+ACTIVITY_MODULES_IMPORT_PATHS.append('ulakbus.activities')
 # absolute path to the workflow packages
 WORKFLOW_PACKAGES_PATH = os.path.join(BASE_DIR, 'workflows')
 
 AUTH_BACKEND = 'ulakbus.models.auth.AuthBackend'
 
-# left blank to use StreamHandler aka stderr
-LOG_HANDLER = os.environ.get('LOG_HANDLER', 'file')
+# # left blank to use StreamHandler aka stderr
+# LOG_HANDLER = os.environ.get('LOG_HANDLER', 'file')
+#
+# # logging dir for file handler
+# LOG_DIR = os.environ.get('LOG_DIR', '/tmp/')
 
-# logging dir for file handler
-LOG_DIR = os.environ.get('LOG_DIR', '/tmp/')
-
-DEFAULT_CACHE_EXPIRE_TIME = 99999999  # seconds
+# DEFAULT_CACHE_EXPIRE_TIME = 99999999  # seconds
 
 # workflows that dosen't require logged in user
 ANONYMOUS_WORKFLOWS = ['simple_login',]
 
-#PYOKO SETTINGS
+# #PYOKO SETTINGS
 DEFAULT_BUCKET_TYPE = 'models'
-RIAK_SERVER = os.environ.get('RIAK_SERVER', 'localhost')
-RIAK_PROTOCOL = os.environ.get('RIAK_PROTOCOL', 'http')
-RIAK_PORT = os.environ.get('RIAK_PORT', 8098)
+# RIAK_SERVER = os.environ.get('RIAK_SERVER', 'localhost')
+# RIAK_PROTOCOL = os.environ.get('RIAK_PROTOCOL', 'http')
+# RIAK_PORT = os.environ.get('RIAK_PORT', 8098)
+#
+# REDIS_SERVER = os.environ.get('REDIS_SERVER')
 
-REDIS_SERVER = os.environ.get('REDIS_SERVER')
-
+#
+# ALLOWED_ORIGINS = ['http://127.0.0.1:8080',
+#                    'http://127.0.0.1:9001',
+#                    'http://ulakbus.zetaops.io',
+#                    'http://ulakbus.org',
+#                    'http://ulakbus.net',
+#                    'http://104.155.6.147']
