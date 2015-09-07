1794afb963a00898e87afccfcbe8c0a8c2c7cfe2
Evren Esat Özkan <evrenesat@zetaops.io>
2015-09-07 15:26:58 +0300

    added create_user command to ulakbus fixed basetestcase user creation

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -1,9 +1,7 @@
 # -*-  coding: utf-8 -*-
 from time import sleep
 from ulakbus.models import User, AbstractRole, Role, Permission
-from zengine.lib.test_utils import BaseTestCase as ZengineBaseTestCase, user_pass, \
-    base_test_permissions
-
+from zengine.lib.test_utils import BaseTestCase as ZengineBaseTestCase, user_pass
 
 class BaseTestCase(ZengineBaseTestCase):
 
@@ -14,7 +12,7 @@ class BaseTestCase(ZengineBaseTestCase):
                                                            username='test_user')
         if new:
             role = Role(user=self.client.user, abstract_role=abs_role).save()
-            for perm in base_test_permissions:
+            for perm in Permission.objects.raw("code:crud* OR code:login* OR code:User*"):
                 permission = Permission(name=perm, code=perm).save()
                 role.Permissions(permission=permission)
             role.save()
