83acb5907ea3897741f8b8e1fc546af2e1c5bcfd
Ali Riza Keles <ali.r.keles@gmail.com>
2015-09-17 14:36:50 +0300

    commented out conf lines

--- gunicorn/gunicorn_config.py
+++ gunicorn/gunicorn_config.py
@@ -188,34 +188,34 @@ proc_name = None
 #limit_request_field_size = 8190 # Limit the allowed size of an HTTP request header field.
 
 
-def post_fork(server, worker):
-    server.log.info("Worker spawned (pid: %s)", worker.pid)
-
-def pre_fork(server, worker):
-    pass
-
-def pre_exec(server):
-    server.log.info("Forked child, re-executing.")
-
-def when_ready(server):
-    server.log.info("Server is ready. Spawning workers")
-
-def worker_int(worker):
-    worker.log.info("worker received INT or QUIT signal")
-
-    ## get traceback info
-    import threading, sys, traceback
-    id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
-    code = []
-    for threadId, stack in sys._current_frames().items():
-        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""),
-            threadId))
-        for filename, lineno, name, line in traceback.extract_stack(stack):
-            code.append('File: "%s", line %d, in %s' % (filename,
-                lineno, name))
-            if line:
-                code.append("  %s" % (line.strip()))
-    worker.log.debug("\n".join(code))
-
-def worker_abort(worker):
-    worker.log.info("worker received SIGABRT signal")
+#def post_fork(server, worker):
+#    server.log.info("Worker spawned (pid: %s)", worker.pid)
+#
+#def pre_fork(server, worker):
+#    pass
+#
+#def pre_exec(server):
+#    server.log.info("Forked child, re-executing.")
+#
+#def when_ready(server):
+#    server.log.info("Server is ready. Spawning workers")
+#
+#def worker_int(worker):
+#    worker.log.info("worker received INT or QUIT signal")
+#
+#    ## get traceback info
+#    import threading, sys, traceback
+#    id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
+#    code = []
+#    for threadId, stack in sys._current_frames().items():
+#        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""),
+#            threadId))
+#        for filename, lineno, name, line in traceback.extract_stack(stack):
+#            code.append('File: "%s", line %d, in %s' % (filename,
+#                lineno, name))
+#            if line:
+#                code.append("  %s" % (line.strip()))
+#    worker.log.debug("\n".join(code))
+#
+#def worker_abort(worker):
+#    worker.log.info("worker received SIGABRT signal")
