d6ea5c4c4ef5b027d6a248de1bfaab263d63dad6
Ozgur Firat Cinar <ozgurfiratcinar@gmail.com>
2015-09-21 16:32:45 +0300

    hizmetkayitlari zato service adapted new model hierarchy

--- ulakbus/services/personel/hitap/hizmet_cetveli_sorgula_kaydet.py
+++ ulakbus/services/personel/hitap/hizmet_cetveli_sorgula_kaydet.py
@@ -11,7 +11,7 @@ import urllib2
 import socket
 
 os.environ["PYOKO_SETTINGS"] = 'ulakbus.settings'
-from ulakbus.models.personel import Personel
+from ulakbus.models.hitap import HizmetKayitlari
 
 H_USER = os.environ["HITAP_USER"]
 H_PASS = os.environ["HITAP_PASS"]
@@ -24,39 +24,37 @@ class HizmetCetveliSorgula(Service):
 
     def handle(self):
 
-        def pass_service_records(employee_passed_record, records):
-            hizmet_kayitlari = employee_passed_record.HizmetKayitlari()
-
-            # if service_list[i].baslamaTarihi == '01.01.0001': service_list[i].baslamaTarihi = '22.05.1942'
-            # service_records.start_date = service_list[i].baslamaTarihi
-            # if service_list[i].bitisTarihi == '01.01.0001': service_list[i].bitisTarihi = '22.05.1942'
-            # service_records.end_date = service_list[i].bitisTarihi
-
-            hizmet_kayitlari.emekli_derece = records['emekli_derece']
-            hizmet_kayitlari.emekli_kademe = records['emekli_kademe']
-            hizmet_kayitlari.gorev = records['gorev']
-            hizmet_kayitlari.unvan_kod = records['unvan_kod']
-            hizmet_kayitlari.hizmet_sinifi = records['hizmet_sinifi']
-            hizmet_kayitlari.kayit_no = records['kayit_no']
-            hizmet_kayitlari.kazanilmis_hak_ayligi_derece = records['kazanilmis_hak_ayligi_derece']
-            hizmet_kayitlari.kazanilmis_hak_ayligi_kademe = records['kazanilmis_hak_ayligi_kademe']
-            hizmet_kayitlari.odeme_derece = records['odeme_derece']
-            hizmet_kayitlari.odeme_kademe = records['odeme_kademe']
-            hizmet_kayitlari.emekli_ekgosterge = records['emekli_ek_gosterge']
-            hizmet_kayitlari.kadro_derece = records['kadro_derece']
-            hizmet_kayitlari.kazanilmis_hak_ayligi_ekgosterge = records['kazanilmis_hak_ayligi_ekgosterge']
-            hizmet_kayitlari.odeme_ek_gosterge = records['odeme_ek_gosterge']
-            hizmet_kayitlari.sebep_kod = records['sebep_kod']
-            hizmet_kayitlari.tckn = records['tckn']
+        def pass_hizmet_kayitlari(hizmet_kayitlari_passed, record_values):
+            hizmet_kayitlari_passed.baslama_tarihi = record_values['baslama_tarihi']
+            hizmet_kayitlari_passed.bitis_tarihi = record_values['bitis_tarihi']
+            hizmet_kayitlari_passed.emekli_derece = record_values['emekli_derece']
+            hizmet_kayitlari_passed.emekli_kademe = record_values['emekli_kademe']
+            hizmet_kayitlari_passed.gorev = record_values['gorev']
+            hizmet_kayitlari_passed.unvan_kod = record_values['unvan_kod']
+            hizmet_kayitlari_passed.hizmet_sinifi = record_values['hizmet_sinifi']
+            hizmet_kayitlari_passed.kayit_no = record_values['kayit_no']
+            hizmet_kayitlari_passed.kazanilmis_hak_ayligi_derece = record_values[
+                'kazanilmis_hak_ayligi_derece']
+            hizmet_kayitlari_passed.kazanilmis_hak_ayligi_kademe = record_values[
+                'kazanilmis_hak_ayligi_kademe']
+            hizmet_kayitlari_passed.odeme_derece = record_values['odeme_derece']
+            hizmet_kayitlari_passed.odeme_kademe = record_values['odeme_kademe']
+            hizmet_kayitlari_passed.emekli_ekgosterge = record_values['emekli_ek_gosterge']
+            hizmet_kayitlari_passed.kadro_derece = record_values['kadro_derece']
+            hizmet_kayitlari_passed.kazanilmis_hak_ayligi_ekgosterge = record_values[
+                'kazanilmis_hak_ayligi_ekgosterge']
+            hizmet_kayitlari_passed.odeme_ekgosterge = record_values['odeme_ekgosterge']
+            hizmet_kayitlari_passed.sebep_kod = record_values['sebep_kod']
+            hizmet_kayitlari_passed.tckn = record_values['tckn']
             try:
-                hizmet_kayitlari.ucret = float(records['ucret'].strip())
+                hizmet_kayitlari_passed.ucret = float(record_values['ucret'].strip())
             except ValueError:
                 pass
             try:
-                hizmet_kayitlari.yevmiye = float(records['yevmiye'].strip())
+                hizmet_kayitlari_passed.yevmiye = float(record_values['yevmiye'].strip())
             except ValueError:
                 pass
-            hizmet_kayitlari.kurum_onay_tarihi = records['kurum_onay_tarihi']
+            hizmet_kayitlari_passed.kurum_onay_tarihi = record_values['kurum_onay_tarihi']
 
         tckn = self.request.payload['personel']['tckn']
         conn = self.outgoing.soap['HITAP'].conn
@@ -64,43 +62,51 @@ class HizmetCetveliSorgula(Service):
         # connects with soap client to the HITAP
         try:
             with conn.client() as client:
-                service_bean = client.service.HizmetCetvelSorgula(H_USER, H_PASS, tckn).HizmetCetveliServisBean
+                service_bean = client.service.HizmetCetvelSorgula(H_USER, H_PASS,
+                                                                  tckn).HizmetCetveliServisBean
                 self.logger.info("zato service started to work.")
 
-                # collects data from HITAP {record_id: record_values_belong_to_that_record_id}
                 hitap_dict = {}
                 for record in range(0, len(service_bean)):
                     hitap_dict[service_bean[record].kayitNo] = {
-                        'baslama_tarihi': service_bean[record].baslamaTarihi,
-                        'bitis_tarihi': service_bean[record].bitisTarihi,
+                        'baslama_tarihi': '01.01.1900' if
+                        service_bean[record].baslamaTarihi == "01.01.0001" else
+                        service_bean[record].baslamaTarihi,
+                        'bitis_tarihi': '01.01.1900' if
+                        service_bean[record].bitisTarihi == "01.01.0001" else
+                        service_bean[record].bitisTarihi,
                         'emekli_derece': service_bean[record].emekliDerece,
                         'emekli_kademe': service_bean[record].emekliKademe,
                         'gorev': service_bean[record].gorev,
                         'unvan_kod': service_bean[record].unvanKod,
                         'hizmet_sinifi': service_bean[record].hizmetSinifi,
                         'kayit_no': service_bean[record].kayitNo,
-                        'kazanilmis_hak_ayligi_derece': service_bean[record].kazanilmisHakAyligiDerece,
-                        'kazanilmis_hak_ayligi_kademe': service_bean[record].kazanilmisHakAyligiKademe,
+                        'kazanilmis_hak_ayligi_derece': service_bean[
+                            record].kazanilmisHakAyligiDerece,
+                        'kazanilmis_hak_ayligi_kademe': service_bean[
+                            record].kazanilmisHakAyligiKademe,
                         'odeme_derece': service_bean[record].odemeDerece,
                         'odeme_kademe': service_bean[record].odemeKademe,
                         'emekli_ek_gosterge': service_bean[record].emekliEkGosterge,
                         'kadro_derece': service_bean[record].kadroDerece,
-                        'kazanilmis_hak_ayligi_ekgosterge': service_bean[record].kazanilmisHakAyligiEkGosterge,
-                        'odeme_ek_gosterge': service_bean[record].odemeEkGosterge,
+                        'kazanilmis_hak_ayligi_ekgosterge': service_bean[
+                            record].kazanilmisHakAyligiEkGosterge,
+                        'odeme_ekgosterge': service_bean[record].odemeEkGosterge,
                         'sebep_kod': service_bean[record].sebepKod,
                         'tckn': service_bean[record].tckn,
                         'ucret': service_bean[record].ucret,
                         'yevmiye': service_bean[record].yevmiye,
-                        'kurum_onay_tarihi': service_bean[record].kurumOnayTarihi
+                        'kurum_onay_tarihi': '01.01.1900' if
+                        service_bean[record].kurumOnayTarihi == "01.01.0001" else service_bean[
+                            record].kurumOnayTarihi
                     }
-
                 self.logger.info("hitap_dict created.")
 
                 # if employee saved before, find that and add new records from hitap to riak
                 try:
                     riak_dict_from_db_queries_with_pno = {}
-                    employee = Personel.objects.filter(hizmet_kayitlari__tckn=tckn).get()
-                    for record in employee.HizmetKayitlari:
+                    # hizmet_kayitlari_list = HizmetKayitlari.objects.filter(tckn=tckn)
+                    for record in HizmetKayitlari.objects.filter(tckn=tckn):
                         riak_dict_from_db_queries_with_pno[record.kayit_no] = {
                             'baslama_tarihi': record.baslama_tarihi,
                             'bitis_tarihi': record.bitis_tarihi,
@@ -116,56 +122,45 @@ class HizmetCetveliSorgula(Service):
                             'odeme_kademe': record.odeme_kademe,
                             'emekli_ekgosterge': record.emekli_ekgosterge,
                             'kadro_derece': record.kadro_derece,
-                            'kazanilmis_hak_ayligi_ekgosterge': record.kazanilmis_hak_ayligi_ekgosterge,
-                            'odeme_ek_gosterge': record.odeme_ek_gosterge,
+                            'kazanilmis_hak_ayligi_ekgosterge':
+                                record.kazanilmis_hak_ayligi_ekgosterge,
+                            'odeme_ekgosterge': record.odeme_ekgosterge,
                             'sebep_kod': record.sebep_kod,
                             'tckn': record.tckn,
                             'ucret': record.ucret,
                             'yevmiye': record.yevmiye,
                             'kurum_onay_tarihi': record.kurum_onay_tarihi
                         }
-
                     self.logger.info("riak_dict_from_db_queries_with_pno created.")
 
-                    for item in employee.HizmetKayitlari:
-                        if not hitap_dict.has_key(item.kayit_no):
-                            self.logger.info("item key: %s " % item.kayit_no)
-                            item.remove()
-                            self.logger.info("Service record deleted.")
-
-                    for hitap_key, hitap_values in hitap_dict.items():
-                        if not riak_dict_from_db_queries_with_pno.has_key(hitap_key):
-                            pass_service_records(employee, hitap_values)
-
                     # if any record exists in riak but not in hitap delete it
-                    employee.save()
-                    self.logger.info("RIAK KEY: %s " % employee.key)
-                    self.logger.info("employee saved. OLD RECORD")
+                    for record in HizmetKayitlari.objects.filter(tckn=tckn):
+                        if record.kayit_no not in hitap_dict:
+                            record.delete()
+                            self.logger.info(
+                                "Hizmet record deleted. Record No: %s" % record.kayit_no)
+
+                    # if any record comes from hitap and not in riak, save it to riak
+                    for hitap_key, hitap_values in hitap_dict.items():
+                        if hitap_key not in riak_dict_from_db_queries_with_pno:
+                            hizmet_kayitlari = HizmetKayitlari()
+                            pass_hizmet_kayitlari(hizmet_kayitlari, hitap_values)
+                            hizmet_kayitlari.save()
+                            self.logger.info("HizmetKayitlari updated")
+
+                    self.logger.info("Service runned.")
 
                 except IndexError:
-                    employee = Personel()
-                    employee.tckn = tckn
-                    for record_id, record_values in hitap_dict.items():
-                        pass_service_records(employee, record_values)
-                        employee.save()
-                        sleep(1)
-                        self.logger.info("RIAK KEY: %s " % employee.key)
-                        self.logger.info("employee saved. NEW RECORD (Index Error)")
-                    sleep(1)
-                except KeyError:
-                    employee = Personel()
-                    employee.tckn = tckn
-                    for record_id, record_values in hitap_dict.items():
-                        pass_service_records(employee, record_values)
-                        employee.save()
-                        sleep(1)
-                        self.logger.info("RIAK KEY: %s " % employee.key)
-                        self.logger.info("employee saved. NEW RECORD (Key Error)")
+                    hizmet_kayitlari = HizmetKayitlari()
+                    for hitap_keys, hitap_values in hitap_dict.items():
+                        pass_hizmet_kayitlari(hizmet_kayitlari, hitap_values)
+                        hizmet_kayitlari.save()
+                        self.logger.info("New AskerlikKayitlari saved.")
                     sleep(1)
                 except socket.error:
                     self.logger.info("Riak connection refused!")
 
-        # except AttributeError:
-            # self.logger.info("TCKN should be wrong!")
+        except AttributeError:
+            self.logger.info("TCKN should be wrong!")
         except urllib2.URLError:
             self.logger.info("No internet connection!")
