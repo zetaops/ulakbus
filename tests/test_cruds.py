fda8ee4dfd4d323ebf7f6ee7ed81eff5460b53c9
Evren Esat Özkan <sleytr@gmail.com>
2015-10-01 23:55:03 +0300

    refactored list_view. added search support

--- tests/test_cruds.py
+++ tests/test_cruds.py
@@ -18,18 +18,19 @@ class TestCase(BaseTestCase):
 
         # calling the crud view without any model should list available models
         resp = self.client.post()
-        assert resp.json['models'] == [[m.Meta.verbose_name_plural, m.__name__] for m in model_registry.get_base_models()]
+        assert resp.json['models'] == [[m.Meta.verbose_name_plural, m.__name__] for m in
+                                       model_registry.get_base_models()]
 
         # calling with just model name (without any cmd) equals to cmd="list"
         resp = self.client.post(model='Personel')
-        assert 'objects' in resp.json
+        assert 'nobjects' in resp.json
 
-        list_objects = resp.json['objects']
-        if list_objects:
-            assert list_objects[0]['data']['ad'] == 'Em1'
+        list_objects = resp.json['nobjects']
+        if list_objects and len(list_objects) > 1:
+            assert list_objects[1][1] == 'Em1'
 
         # count number of records
-        num_of_objects = len(resp.json['objects'])
+        num_of_objects = len(resp.json['nobjects']) - 1
 
         # add a new employee record, then go to list view (do_list subcmd)
         self.client.post(model='Personel', cmd='add')
@@ -39,13 +40,13 @@ class TestCase(BaseTestCase):
                                 form=dict(ad="Em1", tckn="12323121443"))
 
         # we should have 1 more object relative to previous listing
-        assert num_of_objects + 1 == len(resp.json['objects'])
+        assert num_of_objects + 1 == len(resp.json['nobjects']) - 1
 
         # delete the first object then go to list view
         resp = self.client.post(model='Personel',
                                 cmd='delete',
                                 subcmd="do_list",
-                                object_id=resp.json['objects'][0]['key'])
+                                object_id=resp.json['nobjects'][1][0])
 
         # number of objects should be equal to starting point
-        assert num_of_objects == len(resp.json['objects'])
+        assert num_of_objects == len(resp.json['nobjects']) - 1
