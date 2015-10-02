6ffa4343745bb83c176256af82ef25281917dddb
Evren Esat Özkan <sleytr@gmail.com>
2015-10-02 08:34:51 +0300

    added tckn to Personel.search_fields, added a test for list search

--- tests/test_cruds.py
+++ tests/test_cruds.py
@@ -6,6 +6,7 @@
 #
 # This file is licensed under the GNU General Public License v3
 # (GPLv3).  See LICENSE.txt for details.
+import time
 
 from pyoko.model import model_registry
 from base_test_case import BaseTestCase
@@ -50,3 +51,21 @@ class TestCase(BaseTestCase):
 
         # number of objects should be equal to starting point
         assert num_of_objects == len(resp.json['nobjects']) - 1
+
+class TestCase(BaseTestCase):
+    def test_add_search_filter(self):
+        # setup workflow
+        self.prepare_client('crud')
+        resp = self.client.post(model='Personel')
+        resp = self.client.post(model='Personel', query="1234567")
+        if len(resp.json['nobjects']) < 2:
+            self.client.post(model='Personel', cmd='add')
+            for i in range(9):
+                resp = self.client.post(model='Personel',
+                                        cmd='add',
+                                        subcmd="do_add",
+                                        form=dict(ad="Per%s" % i, tckn="123456789%s" % i))
+            time.sleep(3)
+        resp = self.client.post(model='Personel')
+        resp = self.client.post(model='Personel', query="12345678")
+        assert len(resp.json['nobjects']) > 8
