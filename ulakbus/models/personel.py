114862d0498ac50416c7d0775350b7317bec7326
Ali Riza Keles <ali.r.keles@gmail.com>
2015-10-22 14:28:16 +0300

    pasaport basvuru formu duzenlendi, personel gorevlendirme modelleri eklendi

--- ulakbus/models/personel.py
+++ ulakbus/models/personel.py
@@ -9,6 +9,7 @@
 
 from pyoko.model import Model, ListNode, Node
 from pyoko import field
+from .auth import Unit
 
 
 class Personel(Model):
@@ -28,6 +29,7 @@ class Personel(Model):
 
     def durum(self):
         return self.NufusKayitlari.durum
+
     durum.title = "Durum"
 
     def __unicode__(self):
@@ -55,7 +57,6 @@ class Personel(Model):
         durum = field.Boolean("Durum", index=True)
         sebep = field.Integer("Sebep", index=True)
 
-
         class Meta:
             verbose_name = "Nüfus Bilgileri"
 
@@ -67,4 +68,78 @@ class Personel(Model):
 
         class Meta:
             verbose_name = "Adres Bilgisi"
-            verbose_name_plural  = "Adres Bilgileri"
+            verbose_name_plural = "Adres Bilgileri"
+
+    class KurumIciGorevlendirmeBilgileri(ListNode):
+        gorev_tipi = field.String("Görev Tipi", index=True)
+        kurum_ici_gorev_baslama_tarihi = field.Date("Baslama Tarihi", index=True, format="%d.%m.%Y")
+        kurum_ici_gorev_bitis_tarihi = field.Date("Bitiş Tarihi", index=True, format="%d.%m.%Y")
+        birim = Unit()
+        aciklama = field.String("Aciklama")
+        resmi_yazi_sayi = field.String("Resmi Yazi Sayi")
+        resmi_yazi_tarih = field.Date("Resmi Yazi Tarihi", index=True, format="%d.%m.%Y")
+
+        class Meta:
+            verbose_name = "Kurum Ici Gorevlendirme"
+            verbose_name_plural = "Kurum Ici Gorevlendirmeler"
+            form_grouping = [
+                {
+                    "group_title": "Gorev",
+                    "items": ["gorev_tipi", "kurum_ici_gorev_baslama_tarihi", "kurum_ici_gorev_bitis_tarihi", "birim",
+                              "aciklama"],
+                    "layout": "4",
+                    "collapse": False
+                },
+                {
+                    "group_title": "Resmi Yazi",
+                    "items": ["resmi_yazi_sayi", "resmi_yazi_tarih"],
+                    "layout": "2",
+                    "collapse": False
+                }
+            ]
+
+    class KurumDisiGorevlendirmeBilgileri(ListNode):
+        gorev_tipi = field.String("Görev Tipi", index=True)
+        kurum_disi_gorev_baslama_tarihi = field.Date("Baslama Tarihi", index=True, format="%d.%m.%Y")
+        kurum_disi_gorev_bitis_tarihi = field.Date("Bitiş Tarihi", index=True, format="%d.%m.%Y")
+        aciklama = field.Text("Aciklama")
+        resmi_yazi_sayi = field.String("Resmi Yazi Sayi")
+        resmi_yazi_tarih = field.Date("Resmi Yazi Tarihi", index=True, format="%d.%m.%Y")
+        maas = field.Boolean("Maas")
+        yevmiye = field.Boolean("Yevmiye", default=False)
+        yolluk = field.Boolean("Yolluk", default=False)
+        ulke = field.Integer("Ulke", default="90")
+
+        class Meta:
+            verbose_name = "Kurum Disi Gorevlendirme"
+            verbose_name_plural = "Kurum Disi Gorevlendirmeler"
+            form_grouping = [
+                {
+                    "layout": "4",
+                    "groups": [
+                        {
+                            "group_title": "Gorev",
+                            "items": ["gorev_tipi", "kurum_disi_gorev_baslama_tarihi", "kurum_disi_gorev_bitis_tarihi",
+                                      "ulke",
+                                      "aciklama"],
+                            "collapse": False
+                        }
+                    ]
+
+                },
+                {
+                    "layout": "2",
+                    "groups": [
+                        {
+                            "group_title": "Resmi Yazi",
+                            "items": ["resmi_yazi_sayi", "resmi_yazi_tarih"],
+                            "collapse": False
+                        },
+                        {
+                            "items": ["maas", "yevmiye", "yolluk"],
+                            "collapse": False
+                        }
+                    ]
+
+                },
+            ]
