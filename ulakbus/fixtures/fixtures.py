be2e715774c58fe62e89bb301afa2f42c1018f1f
Ali Riza Keles <ali.r.keles@gmail.com>
2015-10-26 16:55:27 +0200

    some try except etc..

--- ulakbus/fixtures/fixtures.py
+++ ulakbus/fixtures/fixtures.py
@@ -4,14 +4,21 @@ import riak
 from ulakbus import settings
 
 client = riak.RiakClient(protocol=settings.RIAK_PROTOCOL, host=settings.RIAK_SERVER, port=settings.RIAK_PROTOCOL)
-fix_bucket = client.bucket_type('models').bucket('ulakbus_fixtures')
+fixture_bucket = client.bucket_type('models').bucket('ulakbus_fixtures')
+
 FIXTURES_FILE = os.environ.get('FIXTURES_FILE',
                                '/'.join([os.path.dirname(os.path.realpath("__file__")), 'settings.json']))
 
-with open("settings.json", "r") as fixtures_file:
-    fixtures = json.load(fixtures_file)
-    for fix in fixtures:
-        f = fix_bucket.get(fix)
-        f.data = fixtures[fix]
-        print "%s: %s kaydediliyor" % (fix, fixtures[fix])
-        f.store()
+try:
+    with open(FIXTURES_FILE, "r") as fixtures_file:
+        try:
+            fixtures = json.load(fixtures_file)
+            for fix in fixtures:
+                f = fixture_bucket.get(fix)
+                f.data = fixtures[fix]
+                print "%s: %s stored.." % (fix, fixtures[fix])
+                f.store()
+        except ValueError as e:
+            print "please validate your json file: %s" % e
+except IOError:
+    print "file not found: %" % FIXTURES_FILE
