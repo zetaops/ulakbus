9102d529181a409a0c507b6757c49e5e5efd4c37
Evren Esat Özkan <evrenesat@zetaops.io>
2015-10-29 18:04:12 +0300

    yeni personel workflowu

---
+++ ulakbus/views/personel.py
@@ -0,0 +1,27 @@
+# -*-  coding: utf-8 -*-
+"""
+"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+from pyoko.model import field
+from zengine.lib.forms import JsonForm
+from zengine.views.base import SimpleView
+
+class TCKNForm(JsonForm):
+    TYPE_OVERRIDES = {'add': 'submit'}
+    tcno = field.String("TC No")
+    cmd = field.String("Ekle", default='do')
+
+
+
+class YeniPersonelEkle(SimpleView):
+    def show_view(self):
+        self.current.output['forms'] = TCKNForm().serialize()
+
+
+def get_personel_from_hitap(tcno):
+    pass
+
