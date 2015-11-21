3902c9324458dbab6422cfdc01e08341f1a51d91
Ali Riza Keles <ali.r.keles@gmail.com>
2015-11-21 20:48:15 +0200

    kadro islemleri crudview extended

--- ulakbus/views/personel/kadro.py
+++ ulakbus/views/personel/kadro.py
@@ -8,13 +8,55 @@
 # (GPLv3).  See LICENSE.txt for details.
 #
 # Yeni Personel Ekle WF adimlarini icerir.
+from pyoko import form
+from zengine.views.crud import CrudView, obj_filter
 
 from zengine.lib.forms import JsonForm
-from zengine.views.base import SimpleView
 from ulakbus.models.personel import Kadro
 
 
-class SakliKadroEkle(SimpleView):
-    def show_view(self):
-        form = JsonForm(model=Kadro)
-        self.current.output['forms'] = form.serialize()
+class KadroIslemleri(CrudView):
+    class Meta:
+        model = 'Kadro'
+        dispatch = False
+        object_actions = []
+
+    class ObjectForm(JsonForm):
+        class Meta:
+            exclude = ['durum', ]
+
+        save_edit = form.Button("Kaydet")
+
+    # def kadro_ekle_form(self):
+    #     self.object_form.exclude = ['durum',]
+    #     self.form()
+
+    def kadro_kaydet(self):
+        super(KadroIslemleri, self).set_form_data_to_object()
+        self.object.durum = 1
+        self.save()
+
+    def sakli_izinli_degistir(self):
+        self.object.durum = 1
+        self.save()
+
+    @obj_filter
+    def sakli_kadro(self, obj, result):
+        if obj.durum == 1:
+            result['actions'] = [{'name': 'Izinli Yap', 'cmd': 'form', 'mode': 'normal', 'show_as': 'button'}, ]
+        return result
+
+    @obj_filter
+    def izinli_kadro(self, obj, result):
+        if obj.durum == 2:
+            result['actions'] = [{'name': 'Sakli Yap', 'cmd': 'form', 'mode': 'normal', 'show_as': 'button'}, ]
+        return result
+
+    @obj_filter
+    def duzenlenebilir_veya_silinebilir_kadro(self, obj, result):
+        if obj.durum == 2 or obj.durum == 1:
+            result['actions'] = [
+                {'name': 'Sil', 'cmd': 'delete', 'mode': 'bg', 'show_as': 'button'},
+                {'name': 'Düzenle', 'cmd': 'form', 'mode': 'normal', 'show_as': 'button'},
+            ]
+        return result
