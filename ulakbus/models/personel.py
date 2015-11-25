e8ed8ec208b080a4975d7ce17dd2f0b76c9ca196
Evren Esat Özkan <evrenesat@zetaops.io>
2015-11-25 17:26:40 +0200

    added list_fields

--- ulakbus/models/personel.py
+++ ulakbus/models/personel.py
@@ -176,6 +176,7 @@ class KurumDisiGorevlendirmeBilgileri(Model):
     class Meta:
         verbose_name = "Kurum Disi Gorevlendirme"
         verbose_name_plural = "Kurum Disi Gorevlendirmeler"
+        list_fields = ["ulke", "gorev_tipi", "kurum_disi_gorev_baslama_tarihi"]
         form_grouping = [
             {
                 "layout": "4",
