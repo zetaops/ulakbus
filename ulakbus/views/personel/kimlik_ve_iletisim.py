54da8093ff021b1355de77495ba33127f1090e6c
Evren Esat Özkan <evrenesat@zetaops.io>
2015-12-01 14:04:29 +0200

    hatalar düzeltildi ve kod yapısı yeniden düzenlendi

--- ulakbus/views/personel/kimlik_ve_iletisim.py
+++ ulakbus/views/personel/kimlik_ve_iletisim.py
@@ -27,56 +27,63 @@ from ulakbus.services.zato_wrapper import KPSAdresBilgileriGetir
 from pyoko import form
 
 
+class KimlikBilgileriForm(JsonForm):
+    class Meta:
+        include = ['tckn', 'ad', 'soyad', 'cinsiyet', 'uyruk', 'medeni_hali', 'cuzdan_seri',
+                   'cuzdan_seri_no', 'baba_adi', 'ana_adi', 'dogum_tarihi', 'dogum_tarihi',
+                   'dogum_yeri', 'medeni_hali', 'kayitli_oldugu_il', 'kayitli_oldugu_ilce',
+                   'kayitli_oldugu_mahalle_koy',
+                   'kayitli_oldugu_cilt_no', 'kayitli_oldugu_aile_sira_no',
+                   'kayitli_oldugu_sira_no',
+                   'kimlik_cuzdani_verildigi_yer', 'kimlik_cuzdani_verilis_nedeni',
+                   'kimlik_cuzdani_kayit_no',
+                   'kimlik_cuzdani_verilis_tarihi']
+
+    kaydet = form.Button("Kaydet", cmd="save")
+    mernis = form.Button("Mernis'ten Kimlik Bilgileri Getir", cmd="mernis_kimlik_sorgula")
+
+
+class IletisimBilgileriForm(JsonForm):
+    class Meta:
+        include = ['ikamet_adresi', 'ikamet_il', 'ikamet_ilce', 'adres_2', 'adres_2_posta_kodu',
+                   'oda_no', 'oda_tel_no', 'cep_telefonu', 'e_posta', 'e_posta_2', 'e_posta_3',
+                   'web_sitesi']
+
+    kaydet = form.Button("Kaydet", cmd="save")
+    kps = form.Button("KPS'den Adres Bilgileri Getir", cmd="kps_adres_sorgula")
+
+
+class DigerBilgilerForm(JsonForm):
+    class Meta:
+        include = ['yayinlar', 'projeler', 'verdigi_dersler', 'kan_grubu', 'ehliyet',
+                   'unvan', 'biyografi', 'notlar', 'engelli_durumu', 'engel_grubu',
+                   'engel_derecesi',
+                   'engel_orani', 'personel_turu']
+
+    kaydet = form.Button("Kaydet", cmd="save")
+
+
 class KimlikIletisim(CrudView):
     class Meta:
         model = 'Personel'
 
-    class ObjectForm(JsonForm):
-        pass
-
-    def kimlik_bilgileri(self):
-        self.object_form.include = ['tckn', 'ad', 'soyad', 'cinsiyet', 'uyruk', 'medeni_hali', 'cuzdan_seri',
-                                    'cuzdan_seri_no', 'baba_adi', 'ana_adi', 'dogum_tarihi', 'dogum_tarihi',
-                                    'dogum_yeri', 'medeni_hali', 'kayitli_oldugu_il', 'kayitli_oldugu_ilce',
-                                    'kayitli_oldugu_mahalle_koy',
-                                    'kayitli_oldugu_cilt_no', 'kayitli_oldugu_aile_sira_no', 'kayitli_oldugu_sira_no',
-                                    'kimlik_cuzdani_verildigi_yer', 'kimlik_cuzdani_verilis_nedeni',
-                                    'kimlik_cuzdani_kayit_no',
-                                    'kimlik_cuzdani_verilis_tarihi']
-        # self.object_form._ordered_fields.append(('mernis', form.Button("Mernis'ten Kimlik Bilgileri Getir")))
-        self.object_form.mernis = form.Button("Mernis'ten Kimlik Bilgileri Getir",
-                                              cmd="mernis_kimlik_sorgula")
-        self.object_form.kaydet = form.Button("Kaydet", cmd="save")
-        self.form_out()
-
     def mernis_kimlik_sorgula(self):
         zs = MernisKimlikBilgileriGetir(tckn=self.object.tckn)
         response = zs.zato_request()
-        for k, v in response['return']:
-            self.object.__setattribute__(k, v)
-
+        self.object(**response)
         self.object.save()
-
-    def iletisim_bilgileri(self):
-        self.object_form.include = ['ikamet_adresi', 'ikamet_il', 'ikamet_ilce', 'adres_2', 'adres_2_posta_kodu',
-                                    'oda_no', 'oda_tel_no', 'cep_telefonu', 'e_posta', 'e_posta_2', 'e_posta_3',
-                                    'web_sitesi']
-        self.object_form.mernis = form.Button("KPS'den Adres Bilgileri Getir",
-                                              cmd="kps_adres_sorgula")
-        self.object_form.kaydet = form.Button("Kaydet", cmd="save")
-        self.form_out()
 
     def kps_adres_sorgula(self):
         zs = KPSAdresBilgileriGetir(tckn=self.object.tckn)
         response = zs.zato_request()
-        for k, v in response['return']:
-            self.object.__setattribute__(k, v)
-
+        self.object(**response)
         self.object.save()
 
+    def kimlik_bilgileri(self):
+        self.form_out(KimlikBilgileriForm(self.object, current=self.current))
+
+    def iletisim_bilgileri(self):
+        self.form_out(IletisimBilgileriForm(self.object, current=self.current))
+
     def diger_bilgiler(self):
-        self.object_form.include = ['yayinlar', 'projeler', 'verdigi_dersler', 'kan_grubu', 'ehliyet',
-                                    'unvan', 'biyografi', 'notlar', 'engelli_durumu', 'engel_grubu', 'engel_derecesi',
-                                    'engel_orani', 'personel_turu']
-        self.object_form.kaydet = form.Button("Kaydet", cmd="save")
-        self.form_out()
+        self.form_out(self.form_out(DigerBilgilerForm(self.object, current=self.current)))
