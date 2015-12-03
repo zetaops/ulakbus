3e7dd0ee0cc85cd477372a4bf2636ab2d1684c66
Ali Riza Keles <ali.r.keles@gmail.com>
2015-12-03 14:08:11 +0200

    service classes modified to fix inheritance problems

--- ulakbus/services/common/nvi.py
+++ ulakbus/services/common/nvi.py
@@ -23,17 +23,24 @@ if DEBUG:
 
 
 class NVIService(Service):
-    def __init__(self, *ignored_args, **ignored_kwargs):
-        super(NVIService, self).__init__(*ignored_args, **ignored_kwargs)
-        self.service = {}
+    def __init__(self):
+        self.service_action = ""
+        self.service_xml_body = ""
+        self.sso_data = {}
+        super(NVIService, self).__init__()
+
+    def handle(self):
         keys = ['nvi_sso_encrypted_data', 'nvi_sso_key_identifier_path',
                 'nvi_sso_digest_value', 'nvi_sso_signature',
                 'nvi_sso_created', 'nvi_sso_expire']
-        self.sso_data = {}
         for k in keys:
+            self.logger.info("k, v" % k, self.kvdb.conn.get(k))
             self.sso_data.update({k, self.kvdb.conn.get(k)})
         if not all(self.sso_data.values()):
             self.invoke_sso_service()
+
+        response = self.request()
+        self.response.payload = {"status": response.status, "result": json.dumps(response.read())}
 
     def invoke_sso_service(self):
         response = self.invoke('nvi-sts.sts-get-token')
@@ -43,7 +50,9 @@ class NVIService(Service):
 
     def request_xml(self):
         request_xml = """
-        <?xml version="1.0"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+        <?xml version="1.0"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope"
+          xmlns:a="http://www.w3.org/2005/08/addressing"
+          xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
           <s:Header>
             <a:Action s:mustUnderstand="1">http://kps.nvi.gov.tr%s</a:Action>
             <a:MessageID>urn:uuid:%s</a:MessageID>
@@ -51,7 +60,8 @@ class NVIService(Service):
               <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
             </a:ReplyTo>
             <a:To s:mustUnderstand="1">https://kpsv2.nvi.gov.tr/Services/RoutingService.svc</a:To>
-            <o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+            <o:Security s:mustUnderstand="1"
+             xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
               <u:Timestamp u:Id="_0">
                 <u:Created>%s</u:Created>
                 <u:Expires>%s</u:Expires>
@@ -69,19 +79,22 @@ class NVIService(Service):
                 </SignedInfo>
                 <SignatureValue>%s</SignatureValue>
                 <KeyInfo>
-                  <o:SecurityTokenReference k:TokenType="http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV1.1" xmlns:k="http://docs.oasis-open.org/wss/oasis-wss-wssecurity-secext-1.1.xsd">
-                    <o:KeyIdentifier ValueType="http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.0#SAMLAssertionID">%s</o:KeyIdentifier>
+                  <o:SecurityTokenReference
+                    k:TokenType="http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV1.1"
+                      xmlns:k="http://docs.oasis-open.org/wss/oasis-wss-wssecurity-secext-1.1.xsd">
+                    <o:KeyIdentifier
+                      ValueType="http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.0#SAMLAssertionID">%s</o:KeyIdentifier>
                   </o:SecurityTokenReference>
                 </KeyInfo>
               </Signature>
             </o:Security>
-          </s:Header>%s</s:Envelope>""" % (self.service['action'], str(uuid.uuid1()), self.sso_data['nvi_sso_created'],
+          </s:Header>%s</s:Envelope>""" % (self.service_action, str(uuid.uuid1()), self.sso_data['nvi_sso_created'],
                                            self.sso_data['nvi_sso_expire'],
                                            self.sso_data['nvi_sso_encrypted_data'],
                                            self.sso_data['nvi_sso_digest_value'],
                                            self.sso_data['nvi_sso_signature'],
                                            self.sso_data['nvi_sso_key_identifier_path'],
-                                           self.service['body'])
+                                           self.service_xml_body)
         return request_xml
 
     def request(self):
@@ -100,10 +113,10 @@ class KisiSorgulaTCKimlikNo(NVIService):
 
     def handle(self):
         tckn = self.request.payload['tckn']
-        self.service = {
-            "action": "/2011/01/01/KisiSorgulaTCKimlikNoServis/ListeleCoklu",
-            "body": """
-              <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
+        self.service_action = "/2011/01/01/KisiSorgulaTCKimlikNoServis/ListeleCoklu"
+        self.service_xml_body = """
+            <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope"
+              xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
                   <ns1:ListeleCoklu>
                       <ns1:kriterListesi>
                           <ns1:KisiSorgulaTCKimlikNoSorguKriteri>
@@ -111,11 +124,8 @@ class KisiSorgulaTCKimlikNo(NVIService):
                           </ns1:KisiSorgulaTCKimlikNoSorguKriteri>
                       </ns1:kriterListesi>
                   </ns1:ListeleCoklu>
-              </env:Body>""" % tckn
-        }
-        response = self.request()
-
-        self.response.payload = {"status": response.status, "result": json.dumps(response.read())}
+            </env:Body>""" % tckn
+        super(KisiSorgulaTCKimlikNo, self).handle()
 
 
 class CuzdanSorgulaTCKimlikNo(NVIService):
@@ -125,10 +135,10 @@ class CuzdanSorgulaTCKimlikNo(NVIService):
 
     def handle(self):
         tckn = self.request.payload['tckn']
-        self.service = {
-            "action": "/2014/09/01/CuzdanSorgulaTCKimlikNoServis/ListeleCoklu",
-            "body": """
-              <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://kps.nvi.gov.tr/2014/09/01">
+        self.service_action = "/2014/09/01/CuzdanSorgulaTCKimlikNoServis/ListeleCoklu"
+        self.service_xml_body = """
+            <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope"
+              xmlns:ns1="http://kps.nvi.gov.tr/2014/09/01">
                 <ns1:ListeleCoklu>
                     <ns1:kriterListesi>
                         <ns1:CuzdanSorgulaTCKimlikNoSorguKriteri>
@@ -138,10 +148,6 @@ class CuzdanSorgulaTCKimlikNo(NVIService):
                     </ns1:kriterListesi>
                 </ns1:ListeleCoklu>
             </env:Body>""" % tckn
-        }
-        response = self.request()
-
-        self.response.payload = {"status": response.status, "result": json.dumps(response.read())}
 
 
 class YabanciKisiSorgula(NVIService):
@@ -151,10 +157,10 @@ class YabanciKisiSorgula(NVIService):
 
     def handle(self):
         tckn = self.request.payload['tckn']
-        self.service = {
-            "action": "/2013/06/01/YbKisiSorgulaYbKimlikNoServis/ListeleCoklu",
-            "body": """
-              <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns2="http://kps.nvi.gov.tr/2013/06/01" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
+        self.service_action = "/2013/06/01/YbKisiSorgulaYbKimlikNoServis/ListeleCoklu"
+        self.service_xml_body = """
+              <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope"
+                    xmlns:ns2="http://kps.nvi.gov.tr/2013/06/01" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
                   <ns2:ListeleCoklu>
                       <ns2:kriterListesi>
                           <ns1:YbKisiSorgulaYbKimlikNoSorguKriteri>
@@ -163,22 +169,20 @@ class YabanciKisiSorgula(NVIService):
                       </ns2:kriterListesi>
                   </ns2:ListeleCoklu>
               </env:Body>""" % tckn
-        }
-        response = self.request()
-
-        self.response.payload = {"status": response.status, "result": json.dumps(response.read())}
+        super(YabanciKisiSorgula, self).handle()
 
 
 class AdresSorgula(NVIService):
     """
-    NVI Kimlik Bilgileri Servisi
+    Adres Sorgulama
     """
 
     def handle(self):
         tckn = self.request.payload['tckn']
-        self.service = {
-            "action": "/2015/07/01/KimlikNoSorgulaAdresServis/Sorgula",
-            "body": """<env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns2="http://kps.nvi.gov.tr/2015/07/01" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
+        self.service_action = "/2015/07/01/KimlikNoSorgulaAdresServis/Sorgula"
+        self.service_xml_body = """
+            <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope"
+                xmlns:ns2="http://kps.nvi.gov.tr/2015/07/01" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
                     <ns2:Sorgula>
                         <ns2:kriterListesi>
                             <ns1:KimlikNoileAdresSorguKriteri>
@@ -186,32 +190,27 @@ class AdresSorgula(NVIService):
                             </ns1:KimlikNoileAdresSorguKriteri>
                         </ns2:kriterListesi>
                     </ns2:Sorgula>
-                </env:Body>""" % tckn
-        }
-        response = self.request()
-
-        self.response.payload = {"status": response.status, "result": json.dumps(response.read())}
+            </env:Body>""" % tckn
+        super(AdresSorgula, self).handle()
 
 
 class AileBireySorgula(NVIService):
     """
-    NVI Kimlik Bilgileri Servisi
+    Aile Birey Sorgulama
     """
 
     def handle(self):
         tckn = self.request.payload['tckn']
-        self.service = {
-            "action": "/2011/01/01/AileListesiAraTCNoServis/Sorgula",
-            "body": """<env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
-                    <ns1:Sorgula>
-                        <ns1:kriterListesi>
-                            <ns1:AileListesiAraTCNoSorguKriteri>
-                                <ns1:KimlikNo>%s</ns1:KimlikNo>
-                            </ns1:AileListesiAraTCNoSorguKriteri>
-                        </ns1:kriterListesi>
-                    </ns1:Sorgula>
-                </env:Body>""" % tckn
-        }
-        response = self.request()
+        self.service_action = "/2011/01/01/AileListesiAraTCNoServis/Sorgula"
+        self.service_xml_body = """
+            <env:Body xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://kps.nvi.gov.tr/2011/01/01">
+                <ns1:Sorgula>
+                    <ns1:kriterListesi>
+                        <ns1:AileListesiAraTCNoSorguKriteri>
+                            <ns1:KimlikNo>%s</ns1:KimlikNo>
+                        </ns1:AileListesiAraTCNoSorguKriteri>
+                    </ns1:kriterListesi>
+                </ns1:Sorgula>
+            </env:Body>""" % tckn
 
-        self.response.payload = {"status": response.status, "result": json.dumps(response.read())}
+        super(AileBireySorgula, self).handle()
