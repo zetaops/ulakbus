a6fb5e305384040e446c3fd3a76a0036485bc1e5
Mithat Raşit Özçıkrıkcı <mroz@konya.edu.tr>
2015-12-04 13:27:34 +0200

    Ders ekleme işleminde düzenlemeler

--- ulakbus/views/ders/ders.py
+++ ulakbus/views/ders/ders.py
@@ -31,18 +31,27 @@ class Ders_View(CrudView):
 class ProgramBilgisiForm(JsonForm):
 	class Meta:
 		include = ['program']
+	sec = form.Button("Seç", cmd="program_sec")
 
 class DersBilgileriForm(JsonForm):
 	class Meta:
 		include = ['ad', 'kod', 'tanim', 'aciklama', 'onkosul', 'uygulama_saati', 'teori_saati', 'ects_kredisi',
 		'yerel_kredisi', 'zorunlu', 'ders_dili', 'ders_turu', 'ders_amaci', 'ogrenme_ciktilari', 'ders_icerigi',
 		'ders_kategorisi', 'ders_kaynaklari', 'ders_mufredati', 'verilis_bicimi', 'donem', 'ders_koordinatoru']
-	kaydet = form.Button("Kaydet", cmd="kaydet")
+	kaydet = form.Button("Kaydet", cmd="save")
 	kaydet_yeni_kayit = form.Button("Kaydet/Yeni Kayıt Ekle", cmd="kaydet_yeni_kayit")
 
 class DersEkle(CrudView):
 	class Meta:
 		model = "Ders"
+
+	def program_sec(self):
+		current.task_data['program_id'] = self.object.program.key
+
+	def kaydet_yeni_kayit(self):
+		self.set_form_data_to_object()
+		self.object.program = Program.object.get(current.task_data['program_id'])
+		self.save_object()
 
 	def ders_bilgileri(self):
 		self.form_out(DersBilgileriForm(self.object, current = self.current))
