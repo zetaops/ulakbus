d980371161d2636ff8f5eafa9b3f32b13be2b488
Mucahit Buyukyilmaz <mucahit@konya.edu.tr>
2015-12-05 19:56:06 +0200

    Hitap Hizmet cetveli ve Hitap hizmet birleştirme tablolalarına göre İzin hizmet yılı hesaplama eklendi,

--- ulakbus/views/personel/izin.py
+++ ulakbus/views/personel/izin.py
@@ -5,6 +5,8 @@ from pyoko import form
 
 from zengine.lib.forms import JsonForm
 from ulakbus.models.personel import Personel
+from ulakbus.models.hitap import HizmetKayitlari,HizmetBirlestirme
+from datetime import timedelta,date
 
 class IzinIslemleri(CrudView):
 
@@ -47,18 +49,84 @@ class IzinIslemleri(CrudView):
     def goster(self):
         self.list()
         personel = Personel.objects.get(self.input['personel_id'])
-        izin = self.izin_hesapla(personel)
-        self.output['object'] = { "Adı":personel.ad, "Kalan İzin":"30" }
+        izin_gun = self.hizmet_izin_yil_hesapla(personel)
 
-        # Formun üzerinde gösterilen bilgilerin başlığı
-        # set edilmezse {add=null} görünüyor
-        self.output['forms']['model'] = personel.ad + " izin bilgileri"
+        # Personelin izin gün sayısı 365 günden azsa eklemeyi kaldır.
+        # Personel pasifse eklemeyi kaldır.
+        if izin_gun<365 or not self.personel_aktif:
+            del self.output['forms']['schema']['properties']['add']
 
-    def izin_hesapla(self,personel):
-        from datetime import datetime
-        self.ge
-        for izin in self.output['objects']:
-            izin.fields
-            print
+        ## ToDo: Personel bilgileri tabloda gösterilecek
+        self.output['object'] = {
+                               "type": "table",
+                               "fields": [
+                                {
+                                 "name": izin_gun,
+                                 "surname": "Aktif" if self.personel_aktif else "Pasif"
+                                },{
+                                 "name": "ali riza",
+                                 "surname": "keles"
+                                }
+                               ]
+                              }
 
-        pass
+    def emekli_sandigi_hesapla(self,personel):
+        personel_hizmetler = HizmetKayitlari.objects.filter(tckn=personel.tckn)
+
+        baslangic_liste = set()
+        bitis_liste = set()
+
+        for hizmet in personel_hizmetler:
+            baslangic_liste.add(hizmet.baslama_tarihi)
+            bitis_liste.add(hizmet.bitis_tarihi)
+
+        baslangic_liste.remove(date( 1900, 1, 1))
+        bitis_liste.remove(date(1900, 1, 1))
+
+        baslangic_liste = sorted(baslangic_liste)
+        bitis_liste = sorted(bitis_liste)
+
+        ZERO = timedelta(0)
+        toplam_sure = ZERO
+        son_baslama = None
+
+        for baslangic in baslangic_liste:
+            if son_baslama == None:
+                son_baslama = baslangic
+            elif len(bitis_liste)>0 and baslangic >= bitis_liste[0]:
+                toplam_sure += bitis_liste[0] - son_baslama
+                son_baslama = baslangic
+                bitis_liste.remove(bitis_liste[0])
+
+        if son_baslama == None:
+            personel_aktif = False
+        elif len(bitis_liste)>0:
+            personel_aktif = False
+            toplam_sure += bitis_liste[0] - son_baslama
+        else:
+            personel_aktif = True
+            toplam_sure += date.today() - son_baslama
+
+        print str(int(toplam_sure.days / 360)) + " Yıl, " + str(int(toplam_sure.days % 360)) + " Gün"
+        return toplam_sure.days,personel_aktif
+
+    def sgk_hesapla(self,personel):
+        personel_birlestirmeler = HizmetBirlestirme.objects.filter(tckn=personel.tckn)
+        toplam_gun = 0
+
+        for hizmet in personel_birlestirmeler:
+            toplam_gun += hizmet.sure
+
+        return toplam_gun
+
+    def hizmet_izin_yil_hesapla(self,personel):
+        emekli_sandigi_gun,aktif = self.emekli_sandigi_hesapla(personel)
+        sgk_gun = self.sgk_hesapla(personel)
+        self.personel_aktif = aktif
+        return emekli_sandigi_gun + sgk_gun
+
+    def kullanilan_izin_hesapla(self):
+        query = self._apply_list_queries(self.object.objects.filter())
+        izinler = list()
+        # for izin in query:
+        #     if izin. izinler
