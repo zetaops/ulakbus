1f77c17572d42af524ba510e5b30a6bfea0f50ba
drlinux <ibrahim@drlinux.org>
2015-12-10 16:10:21 +0200

    FIX Okutman model unicode

--- ulakbus/models/ogrenci.py
+++ ulakbus/models/ogrenci.py
@@ -63,13 +63,11 @@ class Okutman(Model):
 
     @property
     def okutman(self):
-        return self.personel if self.personel else self.harici_okutman
+        return self.personel if self.personel.key else self.harici_okutman
+
 
     def __unicode__(self):
-        if self.personel:
-            return '%s %s' % (self.personel.ad, self.personel.soyad)
-        else:
-            return '%s %s' % (self.ad, self.soyad)
+        return '%s %s' % (self.okutman.ad, self.okutman.soyad)
 
 
 class Donem(Model):
