b6d2dd700a0bd424ab0528f14f6f4ee26a5c046e
Ali Riza Keles <ali.r.keles@gmail.com>
2015-12-18 13:19:26 +0200

    not giris, wf diagram ve bos metodlar

--- ulakbus/views/ders/ders.py
+++ ulakbus/views/ders/ders.py
@@ -10,10 +10,11 @@
 from pyoko import ListNode
 from zengine import forms
 from zengine.forms import fields
-from zengine.views.crud import CrudView
-from ulakbus.models.ogrenci import Program, Okutman
-from ulakbus.models.ogrenci import Ders
-from ulakbus.models.ogrenci import Sube
+from zengine.views.crud import CrudView, form_modifier
+from ulakbus.models.ogrenci import Program, Okutman, DegerlendirmeNot, Ders, Sube, Sinav, OgrenciDersi, Donem
+from ulakbus.models.ogrenci import DegerlendirmeNot, Ogrenci, OgrenciProgram
+from pyoko.exceptions import ObjectDoesNotExist
+from collections import OrderedDict
 
 
 def prepare_choices_for_model(model, **kwargs):
@@ -66,7 +67,7 @@ class DersEkle(CrudView):
         self.form_out(ProgramBilgisiForm(self.object, current=self.current))
 
 
-class BosForm(forms.JsonForm):
+class SecimForm(forms.JsonForm):
     sec = fields.Button("Sec", cmd="ders_sec")
 
 
@@ -87,6 +88,15 @@ class SubelendirmeForm(forms.JsonForm):
         kontenjan = fields.Integer('Sube Kontenjani')
         dis_kontenjan = fields.Integer('Sube Dis Kontenjani')
         okutman = fields.String('Okutman', choices=okutman_choices)
+
+
+class NotGirisForm(forms.JsonForm):
+    class Ogrenciler(ListNode):
+        ogrenci_no = fields.String('No')
+        ad_soyad = fields.String('Ad Soyad')
+        degerlendirme = fields.Integer('Not')
+        aciklama = fields.String('Aciklama')
+        key = fields.String('KKKKey', hidden=True)
 
 
 class DersSubelendirme(CrudView):
@@ -177,6 +187,214 @@ class DersSubelendirme(CrudView):
     def bilgi_ver(self):
         sbs = Sube.objects.filter(ders_id=self.current.task_data['ders_key'])
         okutmanlar = [s.okutman.__unicode__() for s in sbs]
-        self.current.set_message(type='info', title='Mesaj Iletildi',
-                                 msg='Şubelendirme Bilgileri şu hocalara iletildi: %s' % ", ".join(
-                                     okutmanlar))
+
+        self.current.output['msgbox'] = {
+            'type': 'info', "title": 'Mesaj Iletildi',
+            "msg": 'Şubelendirme Bilgileri şu hocalara iletildi: %s' % ", ".join(okutmanlar)}
+
+
+class NotGirisi(CrudView):
+    class Meta:
+        model = "DegerlendirmeNot"
+
+    def ders_secim(self):
+        _form = forms.JsonForm(current=self.current, title="Ders Seçim Formu")
+        user = self.current.user
+        subeler = Sube.objects.filter(okutman_id=self.get_okutman_key)
+        _form.sube = fields.Integer("Sube Seçiniz",
+                                    choices=prepare_choices_for_model(Sube, okutman_id=self.get_okutman_key))
+        _form.sec = fields.Button("Seç", cmd="Ders Şubesi Seçin")
+        self.form_out(_form)
+
+    def sinav_sec(self):
+        _form = forms.JsonForm(current=self.current, title="Sınav Seçim Formu")
+
+        try:
+            sube_key = self.current.input['form']['sube']
+        except:
+            sube_key = self.current.task_data["sube"]
+
+        _form.sinav = fields.Integer("Sınav Seçiniz", choices=prepare_choices_for_model(Sinav, sube_id=sube_key))
+        self.current.task_data["sube"] = sube_key
+        _form.sec = fields.Button("Seç", cmd="Sınav Seçin")
+        self.form_out(_form)
+
+    def sinav_kontrol(self):
+        sinav_key = self.current.input['form']['sinav']
+        self.current.task_data['sinav_key'] = sinav_key
+        sinav = Sinav.objects.get(sinav_key)
+        self.current.task_data['sinav_degerlendirme'] = sinav.degerlendirme
+
+    def not_girisi(self):
+        _form = NotGirisForm(current=self.current, title="Not Giriş Formu")
+        sinav_key = self.current.task_data['sinav_key']
+        sube_key = self.current.task_data["sube"]
+        sinav = Sinav.objects.get(sinav_key)
+
+        try:
+            ogrenciler = self.current.task_data["notlar"]
+            for ogr in ogrenciler:
+                _form.Ogrenciler(ad_soyad='%s' % (ogr['ad_soyad']),
+                                 ogrenci_no=ogr['ogrenci_no'], degerlendirme=ogr['degerlendirme'],
+                                 aciklama=ogr['aciklama'],
+                                 key=ogr['key'])
+        except:
+            ogrenciler = OgrenciDersi.objects.filter(ders_id=sube_key)
+
+            for ogr in ogrenciler:
+                try:
+                    degerlendirme = DegerlendirmeNot.objects.get(sinav=sinav, ogrenci=ogr.ogrenci_program.ogrenci)
+                    puan = degerlendirme.puan
+                    aciklama = degerlendirme.aciklama
+                    degerlendirme_key = degerlendirme.key
+                except:
+                    puan = 0
+                    aciklama = ""
+                    degerlendirme_key = ""
+
+                _form.Ogrenciler(ad_soyad='%s %s' % (ogr.ogrenci_program.ogrenci.ad, ogr.ogrenci_program.ogrenci.soyad),
+                                 ogrenci_no=ogr.ogrenci_program.ogrenci_no, degerlendirme=puan, aciklama=aciklama,
+                                 key=degerlendirme_key)
+
+        _form.kaydet = fields.Button("Önizleme", cmd="not_kontrol")
+        self.form_out(_form)
+        self.current.output["meta"]["allow_actions"] = False
+
+    def not_kontrol(self):
+        _form = forms.JsonForm(current=self.current, title="Not Önizleme Ekranı")
+
+        try:  # Eğer not_kontrol aşamasından geri dönülmemişse öğrenci notlarını için formdan gelen veriyi kullan
+            ogrenci_notlar = self.current.input['form']['Ogrenciler']
+            self.current.task_data["notlar"] = ogrenci_notlar
+
+            notlar = []
+            for ogr in ogrenci_notlar:
+                ogrnot = OrderedDict({})
+                ogrnot['Öğrenci No'] = ogr['ogrenci_no']
+                ogrnot['Adı Soyadı'] = ogr['ad_soyad']
+                ogrnot['Değerlendirme'] = ogr['degerlendirme']
+                ogrnot['Açıklama'] = ogr['aciklama']
+                notlar.append(ogrnot)
+
+        except:  # Eğer not_kontrol aşamasından geri dönülmüşse task_data'dan gelen not verilerini kullan
+            sinav_key = self.current.task_data['sinav_key']
+            sube_key = self.current.task_data["sube"]
+            sinav = Sinav.objects.get(sinav_key)
+            ogrenciler = OgrenciDersi.objects.filter(ders_id=sube_key)
+            notlar = []
+
+            for ogr in ogrenciler:
+                try:  # Öğrenciye ait notlar daha önceden girilmiş mi?
+                    degerlendirme = DegerlendirmeNot.objects.get(sinav=sinav, ogrenci=ogr.ogrenci_program.ogrenci)
+                    puan = degerlendirme.puan
+                    aciklama = degerlendirme.aciklama
+                except:
+                    puan = 0
+                    aciklama = ""
+
+                ogrnot = OrderedDict({})
+                ogrnot['Öğrenci No'] = ogr.ogrenci_program.ogrenci_no
+                ogrnot['Adı Soyadı'] = '%s %s' % (ogr.ogrenci_program.ogrenci.ad, ogr.ogrenci_program.ogrenci.soyad)
+                ogrnot['Değerlendirme'] = puan
+                ogrnot['Açıklama'] = aciklama
+                notlar.append(ogrnot)
+
+        # Eğer notlar okutman tarından onaylanmışsa (teslim edilmişse) uyarı göster
+
+        if self.current.task_data['sinav_degerlendirme']:
+            self.current.output['msgbox'] = {
+
+                'type': 'info', "title": 'Notlar Onaylandı',
+                "msg": 'Bu derse ait notlar onaylanmış olduğu için içeriği değiştirilemez.'
+
+            }
+
+        else:  # Eğer notlar hala onaylanmamışsa (teslim edilmemişse) form düğmelerini göster
+
+            _form.not_onay = fields.Boolean("Sınav Notlarını Onaylıyorum (Bu işlem geri alınmaz!)")
+            _form.not_duzenle = fields.Button("Notları Düzenle", cmd="not_girisi", flow="not_giris_formuna_don")
+            _form.kaydet = fields.Button("Kaydet", cmd="not_kaydet", flow="end")
+            _form.kaydet_ve_ders_sec = fields.Button("Kaydet ve Ders Seçim Ekranına Dön", cmd="ders_sec",
+                                                     flow="ders_adimina_don")
+            _form.kaydet_ve_sinav_sec = fields.Button("Kaydet ve Sınav Seçim Ekranına Dön", cmd="sinav_sec",
+                                                      flow="sinav_adimina_don")
+
+        self.current.output['object'] = {
+            "type": "table-multiRow",
+            "fields": notlar,
+            "actions": False
+        }
+        self.form_out(_form)
+
+    def not_kaydet(self):
+        term = Donem.objects.filter(guncel=True)[0]
+        sinav_key = self.current.task_data["sinav_key"]
+        sube_key = self.current.task_data["sube"]
+
+        sinav = Sinav.objects.get(sinav_key)
+        ders = sinav.ders
+
+        for ogrenci_not in self.current.task_data["notlar"]:
+
+            try:
+                ogr_data = OgrenciProgram.objects.get(ogrenci_no=ogrenci_not['ogrenci_no'])
+
+                if ogrenci_not['key']:
+                    ogr_not = DegerlendirmeNot.objects.get(ogrenci_not['key'])
+                else:
+                    ogr_not = DegerlendirmeNot()
+
+                ogr_not.puan = ogrenci_not['degerlendirme']
+                ogr_not.aciklama = ogrenci_not['aciklama']
+                ogr_not.ogrenci_no = ogrenci_not['ogrenci_no']
+                ogr_not.donem = '%s' % term.ad
+                ogr_not.yil = term.baslangic_tarihi.year
+                ogr_not.ogretim_elemani = self.get_okutman_name_surname
+                ogr_not.ders = ders
+                ogr_not.sinav = sinav
+                ogr_not.ogrenci = ogr_data.ogrenci
+                ogr_not.save()
+
+            except ObjectDoesNotExist:
+                raise ObjectDoesNotExistError("Ogrenci Bulunamadı %s" % ogrenci_not['ogrenci_no'])
+
+        # Okutman notları onayladığını (teslim ettiğini) bildirmişse
+
+        if self.current.input['form']['not_onay']:
+            sinav.degerlendirme = True
+            sinav.save()
+
+    def kayit_bilgisi_ver(self):
+        import datetime
+        sinav_key = self.current.task_data["sinav_key"]
+        sinav = Sinav.objects.get(sinav_key)
+        sinav_tarihi = sinav.tarih.strftime("%d/%m/%Y")
+
+        self.current.output['msgbox'] = {
+            'type': 'info', "title": 'Notlar Kaydedildi',
+            "msg": '%s dersine ait %s tarihli sınav notları kaydedildi' % (sinav.ders.ad, sinav_tarihi)}
+
+    @property
+    def get_okutman_key(self):
+
+        '''
+        Harici okutman ve okutman kayıt key'lerinin ayrımı için
+        '''
+        return self.current.user.personel.okutman.key if self.current.user.personel.key else self.current.user.harici_okutman.okutman.key
+
+    @property
+    def get_okutman_name_surname(self):
+        '''
+        Harici okutman ve okutman ad,soyad ayrımı için
+        '''
+        return "%s %s" % (self.current.user.personel.okutman.ad,
+                          self.current.user.personel.okutman.soyad) if self.current.user.personel.key else "%s %s" % (
+            self.current.user.harici_okutman.ad, self.current.user.harici_okutman.soyad)
+
+    @form_modifier
+    def not_form_inline_edit(self, serialized_form):
+        '''
+        NotGirisForm'da inline edit elde etmek için
+        '''
+        if 'Ogrenciler' in serialized_form['schema']['properties']:
+            serialized_form['inline_edit'] = ['degerlendirme', 'aciklama']
