d8c8debb798cdc00292926437f1ddd3e29446999
Evren Esat Özkan <evrenesat@zetaops.io>
2015-12-23 11:44:01 +0200

    moved ulkabus specific parts of zengine.views.menu.Menu to ulakbus.views.system.UlakbusMenu

--- ulakbus/views/system.py
+++ ulakbus/views/system.py
@@ -10,6 +10,8 @@ import random
 from uuid import uuid4
 
 from pyoko.conf import settings
+from pyoko.lib.utils import lazy_property, get_object_from_path
+
 from ulakbus.views.reports import ReporterRegistry
 from zengine.views.base import BaseView
 from ulakbus.models import Personel, Ogrenci
@@ -89,6 +91,37 @@ class UlakbusMenu(Menu):
     def __init__(self, current):
         super(UlakbusMenu, self).__init__(current)
         self.add_reporters()
+        self.add_user_data()
+        self.add_settings()
+
+    def add_settings(self):
+        self.output['settings'] = {
+            'static_url': settings.S3_PUBLIC_URL,
+        }
+
+    def add_user_data(self):
+        # add data of current logged in user
+        usr = self.current.user
+        role = self.current.role
+        self.output['current_user'] = {
+            "name": usr.name,
+            "surname": usr.surname,
+            "username": usr.username,
+            "role": self.current.role.abstract_role.name,
+            "avatar": usr.get_avatar_url(),
+            "is_staff": role.is_staff,
+            "is_student": role.is_student,
+            "roles": [{"role": roleset.role.__unicode__()} for roleset in self.current.user.role_set]
+        }
+        if role.is_student:
+            # insert student specific data here
+            self.output['current_user'].update({
+            })
+
+        elif role.is_staff:
+            # insert staff specific data here
+            self.output['current_user'].update({
+            })
 
     def add_reporters(self):
         for mdl in ReporterRegistry.get_reporters():
