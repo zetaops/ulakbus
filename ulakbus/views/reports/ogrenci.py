716634877b74620c5c6e164bbd0d2589cbb8b940
Evren Esat Özkan <evrenesat@zetaops.io>
2015-12-24 13:06:32 +0200

    added room capacity reports

--- ulakbus/views/reports/ogrenci.py
+++ ulakbus/views/reports/ogrenci.py
@@ -48,7 +48,7 @@ class OgrenciByBrithDate(Reporter):
 
 
 class OgrenciHarc(Reporter):
-    TITLE = 'Harc Bilgileri'
+    TITLE = 'Harç Bilgileri'
 
     def get_objects(self):
         # choices = self.convert_choices(Borc().get_choices_for('sebep'))
@@ -58,3 +58,9 @@ class OgrenciHarc(Reporter):
             result["%s %s" % (b.get_sebep_display(), 'Ödenen')] += int(b.odenen_miktar or 0)
 
         return [(k, tl_curreny(v)) for k,v in result.items()]
+
+class RoomCapacities(Reporter):
+    TITLE = 'Kapasitesine Göre Mekanlar'
+
+    def get_objects(self):
+        return Room.objects.distinct_values_of('capacity').items()
