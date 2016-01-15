892c1e9c69f2373dfbf63dec91693f4630e27e90
enesates <enes@enesates.com>
2016-01-15 16:14:44 +0200

    ADD BankaBorc service GH-68, rref #5023, p10 #5023

---
+++ ulakbus/services/common/banka.py
@@ -0,0 +1,51 @@
+# -*-  coding: utf-8 -*-
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+
+
+from zato.server.service import Service
+from ulakbus.models.ogrenci import OgrenciProgram, Borc
+
+
+class BankaBorcService(Service):
+    """
+    Banka Borc Sorgulama Zato Servisi
+    """
+
+    class SimpleIO:
+        input_required = ('banka_kodu', 'sube_kodu', 'kanal_kodu', 'mesaj_no', 'bank_username', 'bank_password',
+                          'ogrenci_no')
+        output_required = ('banka_kodu', 'sube_kodu', 'kanal_kodu', 'mesaj_no', 'bank_username', 'bank_password',
+                           'ogrenci_no', 'ad_soyad', 'ucret_turu', 'tahakkuk_referans_no', 'son_odeme_tarihi',
+                           'borc', 'borc_ack')
+
+    def handle(self):
+        self.get_data()
+
+    def get_data(self):
+        ogrenci_no = self.request.input.ogrenci_no
+
+        ogr = OgrenciProgram.objects.get(ogrenci_no=ogrenci_no).ogrenci
+
+        borclar = Borc.objects.filter(ogrenci=ogr)
+
+        for borc in borclar:
+            borc_sorgu = {
+                'banka_kodu': self.request.input.banka_kodu,
+                'sube_kodu': self.request.input.sube_kodu,
+                'kanal_kodu': self.request.input.kanal_kodu,
+                'mesaj_no': self.request.input.mesaj_no,
+                'bank_username': self.request.input.bank_username,
+                'bank_password': self.request.input.bank_password,
+                'ogrenci_no': self.request.input.ogrenci_no,
+                'ad_soyad': ogr.ad + ogr.soyad,
+                'ucret_turu': borc.sebep,
+                'tahakkuk_referans_no': borc.tahakkuk_referans_no,
+                'son_odeme_tarihi': borc.son_odeme_tarihi,
+                'borc': borc.miktar,
+                'borc_ack': borc.aciklama
+            }
+            self.response.payload.append(borc_sorgu)
