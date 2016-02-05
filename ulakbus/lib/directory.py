0e076611817b6998e5a2818239a4efe84f9d5557
İbrahim YIlmaz <ibrahim@zetaops.io>
2016-02-05 15:17:44 +0200

    ADD create_unitime_export_directory method rrefs #5095 , refs GH-176

--- ulakbus/lib/directory.py
+++ ulakbus/lib/directory.py
@@ -9,3 +9,11 @@
 
 import os
 import datetime
+
+def create_unitime_export_directory():
+    current_date = datetime.datetime.now()
+    directory_name = current_date.strftime('%d_%m_%Y_%H_%M_%S')
+    export_directory = 'bin/dphs/data_exchange/' + directory_name
+    if not os.path.exists(export_directory):
+        os.makedirs(export_directory)
+    return export_directory
