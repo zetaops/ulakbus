03467f396c5abb375d0972ef7200d7ed64dc2b47
İbrahim YIlmaz <ibrahim@zetaops.io>
2016-02-23 11:58:10 +0200

    ADD donem danisman WF için gerekli metodlar ve classlar eklendi rclose #5135, closes GH-192

--- ulakbus/views/ders/danisman.py
+++ ulakbus/views/ders/danisman.py
@@ -21,14 +21,25 @@ from ulakbus.models.auth import Unit
 from collections import OrderedDict
 from ulakbus.views.ders.ders import prepare_choices_for_model
 
+
 class DonemDanismanForm(forms.JsonForm):
-    """``DonemDanisman`` sınıfı için form olarak kullanılacaktır.
+    """``DonemDanismanAtama`` sınıfı için form olarak kullanılacaktır.
 
     """
 
-    fields.Button("İleri")
+    ileri = fields.Button("İleri")
 
-class DonemDanisman(CrudView):
+
+class DonemDanismanListForm(forms.JsonForm):
+    class Okutmanlar(ListNode):
+        secim = fields.Boolean(type="checkbox")
+        ad_soyad = fields.String('Ad Soyad')
+        key = fields.String('Key', hidden=True)
+
+    kaydet = fields.Button("Kaydet")
+
+
+class DonemDanismanAtama(CrudView):
     """Dönem Danışman Atama İş Akışı
 
     Dönem Danışman Atama, aşağıda tanımlı iş akışı adımlarını yürütür.
@@ -69,16 +80,75 @@ class DonemDanisman(CrudView):
 
     def bolum_sec(self):
 
-        unit = self.current.role.unit
-
-
-
+        _unit = self.current.role.unit
+        _form = DonemDanismanForm(current=self, title="Bölüm Seçiniz")
+        _choices = prepare_choices_for_model(Unit, yoksis_no=_unit.yoksis_no)
+        _form.program = fields.Integer(choices=_choices)
+        self.form_out(_form)
 
     def danisman_sec(self):
-        self.output("danisman_sec")
+
+        unit = Unit.objects.get(self.current.input['form']['program'])
+        self.current.task_data['unit_yoksis_no'] = unit.yoksis_no
+        okutmanlar = Okutman.objects.filter(birim_no=unit.yoksis_no)
+        donem = Donem.objects.get(guncel=True)
+        _form = DonemDanismanListForm(current=self, title="Okutman Seçiniz")
+
+        for okt in okutmanlar:
+            try:
+                if DonemDanisman.objects.filter(donem=donem, okutman=okt, bolum=unit):
+                    _form.Okutmanlar(secim=True, ad_soyad='%s %s' % (okt.ad, okt.soyad),
+                                     key=okt.key)
+                else:
+                    _form.Okutmanlar(secim=False, ad_soyad='%s %s' % (okt.ad, okt.soyad),
+                                     key=okt.key)
+            except:
+                pass
+
+        self.form_out(_form)
+        self.current.output["meta"]["allow_actions"] = False
+        self.current.output["meta"]["allow_selection"] = False
 
     def danisman_kaydet(self):
-        self.output("danisman_kaydet")
+        yoksis_no = self.current.task_data['unit_yoksis_no']
+        unit = Unit.objects.get(yoksis_no=yoksis_no)
+        donem = Donem.objects.get(guncel=True)
+        danismanlar = self.current.input['form']['Okutmanlar']
+
+        # Bölümün ilgili dönemine ait bütün danışmanları sil
+        try:
+            for dd in DonemDanisman.objects.filter(donem=donem, bolum=unit):
+                dd.delete()
+        except:
+            pass
+
+        for danisman in danismanlar:
+            if danisman['secim']:
+                key = danisman['key']
+                okutman = Okutman.objects.get(key)
+                donem_danisman = DonemDanisman()
+                donem_danisman.donem = donem
+                donem_danisman.okutman = okutman
+                donem_danisman.bolum = unit
+                try:
+                    donem_danisman.save()
+                except Exception as e:
+                    print(e.message)
 
     def kayit_bilgisi_ver(self):
-        self.output("kayit_bilgisi_ver")
+        yoksis_no = self.current.task_data['unit_yoksis_no']
+        unit = Unit.objects.get(yoksis_no=yoksis_no)
+        donem = Donem.objects.get(guncel=True)
+
+        self.current.output['msgbox'] = {
+            'type': 'info', "title": 'Danismanlar Kaydedildi',
+            "msg": '%s dönemi için %s programına ait danışman listesi kaydedilmiştir' % (
+                donem, unit)}
+
+    @form_modifier
+    def donem_danisman_list_form_inline_edit(self, serialized_form):
+        """DonemDanismanListForm'da seçim alanına inline edit özelliği sağlayan method.
+
+        """
+        if 'Okutmanlar' in serialized_form['schema']['properties']:
+            serialized_form['inline_edit'] = ['secim']
