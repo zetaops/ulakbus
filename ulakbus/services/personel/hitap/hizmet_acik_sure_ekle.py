9989888fc33c84b072e7ea2a59d823672bdeb0b0
İbrahim YIlmaz <ibrahim@zetaops.io>
2016-03-07 14:46:51 +0200

    REFACTOR of HizmetAcikSureEkle class, such as @alirizakeles suggested rrefs #5036, refs GH-151

--- ulakbus/services/personel/hitap/hizmet_acik_sure_ekle.py
+++ ulakbus/services/personel/hitap/hizmet_acik_sure_ekle.py
@@ -16,8 +16,6 @@ Hitap'a personelin açık süre bilgilerinin eklenmesini yapar.
 __author__ = 'H.İbrahim Yılmaz (drlinux)'
 
 from ulakbus.services.personel.hitap.hitap_ekle import HITAPEkle
-from ulakbus.models.hitap import HizmetAcikSure
-
 
 class HizmetAcikSureEkle(HITAPEkle):
     """
@@ -34,32 +32,31 @@ class HizmetAcikSureEkle(HITAPEkle):
                     HizmetAcikSureInsert servisinin alanlarıyla eşlenmektedir.
                     Filtreden geçecek tarih alanları listede tutulmaktadır.
         """
-        key = self.request.payload['key']
-
         self.service_name = 'HizmetAcikSureInsert'
-        hizmet_acik_sure = HizmetAcikSure.objects.get(key)
+
         self.service_dict = {
             'fields': {
-                'tckn': hizmet_acik_sure.tckn,
-                'acikSekil': hizmet_acik_sure.acik_sekil,
-                'iadeSekil': hizmet_acik_sure.iade_sekil,
-                'hizmetDurum': hizmet_acik_sure.hizmet_durum,
-                'husus': hizmet_acik_sure.husus,
-                'acigaAlinmaTarih': hizmet_acik_sure.aciga_alinma_tarih,
-                'goreveSonTarih': hizmet_acik_sure.goreve_son_tarih,
-                'goreveIadeIstemTarih': hizmet_acik_sure.goreve_iade_istem_tarih,
-                'goreveIadeTarih': hizmet_acik_sure.goreve_iade_tarih,
-                'acikAylikBasTarihi': hizmet_acik_sure.acik_aylik_bas_tarih,
-                'acikAylikBitTarihi': hizmet_acik_sure.acik_aylik_bit_tarih,
-                'gorevSonAylikBasTarihi': hizmet_acik_sure.goreve_son_aylik_bas_tarih,
-                'gorevSonAylikBitTarihi': hizmet_acik_sure.goreve_son_aylik_bit_tarih,
-                'SYonetimKaldTarih': hizmet_acik_sure.s_yonetim_kald_tarih,
-                'aciktanAtanmaTarih': hizmet_acik_sure.aciktan_atanma_tarih,
-                'kurumOnayTarihi': hizmet_acik_sure.kurum_onay_tarihi
+                'tckn': self.request.payload['tckn'],
+                'acikSekil': self.request.payload['acik_sekil'],
+                'iadeSekil': self.request.payload['iade_sekil'],
+                'hizmetDurum': self.request.payload['hizmet_durum'],
+                'husus': self.request.payload['husus'],
+                'acigaAlinmaTarih': self.request.payload['aciga_alinma_tarih'],
+                'goreveSonTarih': self.request.payload['goreve_son_tarih'],
+                'goreveIadeIstemTarih': self.request.payload['goreve_iade_istem_tarih'],
+                'goreveIadeTarih': self.request.payload['goreve_iade_tarih'],
+                'acikAylikBasTarihi': self.request.payload['acik_aylik_bas_tarih'],
+                'acikAylikBitTarihi': self.request.payload['acik_aylik_bit_tarih'],
+                'gorevSonAylikBasTarihi': self.request.payload['goreve_son_aylik_bas_tarih'],
+                'gorevSonAylikBitTarihi': self.request.payload['goreve_son_aylik_bit_tarih'],
+                'SYonetimKaldTarih': self.request.payload['s_yonetim_kald_tarih'],
+                'aciktanAtanmaTarih': self.request.payload['aciktan_atanma_tarih'],
+                'kurumOnayTarihi': self.request.payload['kurum_onay_tarihi']
             },
             'date_filter': ['acigaAlinmaTarih', 'goreveSonTarih', 'goreveIadeIstemTarih',
                             'goreveIadeTarih', 'acikAylikBasTarihi', 'acikAylikBitTarihi',
                             'gorevSonAylikBasTarihi', 'gorevSonAylikBitTarihi', 'SYonetimKaldTarih',
-                            'aciktanAtanmaTarih', 'kurumOnayTarihi']
+                            'aciktanAtanmaTarih', 'kurumOnayTarihi'],
+            'required_fields': ['tckn','acikSekil','durum','hizmetDurum','husus','kurumOnayTarihi']
         }
         super(HizmetAcikSureEkle, self).handle()
