59c85aa10a0e93ac73f24c2cf2706dddd6ef1afb
İbrahim YIlmaz <ibrahim@zetaops.io>
2016-03-08 12:17:07 +0200

    REFACTOR of HizmetNufusGuncelle class, required_fields added rrefs #5036, refs GH-151

--- ulakbus/services/personel/hitap/hizmet_nufus_guncelle.py
+++ ulakbus/services/personel/hitap/hizmet_nufus_guncelle.py
@@ -16,8 +16,6 @@ Hitap'a personelin Nufus bilgilerinin guncellenmesini yapar.
 __author__ = 'H.İbrahim Yılmaz (drlinux)'
 
 from ulakbus.services.personel.hitap.hitap_guncelle import HITAPGuncelle
-from ulakbus.models.hitap import NufusKayitlari
-
 
 class HizmetNufusGuncelle(HITAPGuncelle):
     """
@@ -30,35 +28,38 @@ class HizmetNufusGuncelle(HITAPGuncelle):
 
         Attributes:
             service_name (str): İlgili Hitap sorgu servisinin adı
-            service_dict (dict): ''NufusKayitlari'' modelinden gelen kayıtların alanları,
+            service_dict (dict): : Request yoluyla gelen kayıtlar,
                     HizmetNufusUpdate servisinin alanlarıyla eşlenmektedir.
-                    Filtreden geçecek tarih alanları listede tutulmaktadır.
+                    Filtreden geçecek tarih alanları ve servis tarafında gerekli olan
+                    alanlar listede tutulmaktadır.
+
         """
-        key = self.request.payload['key']
 
         self.service_name = 'HizmetNufusUpdate'
-        hizmet_nufus = NufusKayitlari.objects.get(key)
         self.service_dict = {
             'fields': {
-                'kayitNo': hizmet_nufus.kayit_no,
-                'ad': hizmet_nufus.ad,
-                'cinsiyet': hizmet_nufus.cinsiyet,
-                'dogumTarihi': hizmet_nufus.dogum_tarihi,
-                'durum': hizmet_nufus.durum,
-                'emekliSicilNo': hizmet_nufus.emekli_sicil_no,
-                'ilkSoyad': hizmet_nufus.ilk_soy_ad,
-                'kurumSicili': hizmet_nufus.kurum_sicil,
-                'maluliyetKod': hizmet_nufus.maluliyet_kod,
-                'memuriyetBaslamaTarihi': hizmet_nufus.memuriyet_baslama_tarihi,
-                'sebep': hizmet_nufus.sebep,
-                'soyad': hizmet_nufus.soyad,
-                'tckn': hizmet_nufus.tckn,
-                'aciklama': hizmet_nufus.aciklama,
-                'yetkiSeviyesi': hizmet_nufus.yetki_seviyesi,
-                'kurumaBaslamaTarihi': hizmet_nufus.kuruma_baslama_tarihi,
-                'gorevTarihi6495': hizmet_nufus.gorev_tarihi_6495,
-                'emekliSicil6495': hizmet_nufus.emekli_sicil_6495
+                'ad': self.request.payload['ad'],
+                'cinsiyet': self.request.payload['cinsiyet'],
+                'dogumTarihi': self.request.payload['dogum_tarihi'],
+                'durum': self.request.payload['durum'],
+                'emekliSicilNo': self.request.payload['emekli_sicil_no'],
+                'ilkSoyad': self.request.payload['ilk_soy_ad'],
+                'kurumSicili': self.request.payload['kurum_sicil'],
+                'maluliyetKod': self.request.payload['maluliyet_kod'],
+                'memuriyetBaslamaTarihi': self.request.payload['memuriyet_baslama_tarihi'],
+                'sebep': self.request.payload['sebep'],
+                'soyad': self.request.payload['soyad'],
+                'tckn': self.request.payload['tckn'],
+                'aciklama': self.request.payload['aciklama'],
+                'yetkiSeviyesi': self.request.payload['yetki_seviyesi'],
+                'kurumaBaslamaTarihi': self.request.payload['kuruma_baslama_tarihi'],
+                'gorevTarihi6495': self.request.payload['gorev_tarihi_6495'],
+                'emekliSicil6495': self.request.payload['emekli_sicil_6495']
             },
-            'date_filter': ['dogumTarihi', 'memuriyetBaslamaTarihi', 'kurumaBaslamaTarihi']
+            'date_filter': ['dogumTarihi', 'memuriyetBaslamaTarihi', 'kurumaBaslamaTarihi'],
+            'required_fields': ['tckn', 'ad', 'soyad', 'dogumTarihi', 'cinsiyet', 'emekliSicilNo',
+                                'memuriyetBaslamaTarihi', 'durum', 'kurumSicili', 'maluliyetKod',
+                                'sebep', 'yetkiSeviyesi']
+
         }
         super(HizmetNufusGuncelle, self).handle()
