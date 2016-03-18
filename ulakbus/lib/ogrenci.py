35afabbcec7fabc6030ce16f2f2213f054dceb1c
İbrahim YIlmaz <ibrahim@zetaops.io>
2016-03-18 13:37:10 +0200

    ADD OgrenciHelper class'ı ve diploma_notu_uret methodu eklendi, rrefs #5163

--- ulakbus/lib/ogrenci.py
+++ ulakbus/lib/ogrenci.py
@@ -1,5 +1,5 @@
 # -*-  coding: utf-8 -*-
-"""
+"""Öğrenci ile ilgili yardımcı class ve fonksiyonların yer aldığı dosyadır.
 """
 
 # Copyright (C) 2015 ZetaOps Inc.
@@ -8,3 +8,20 @@
 # (GPLv3).  See LICENSE.txt for details.
 
 __author__ = 'H.İbrahim Yılmaz (drlinux)'
+
+from ulakbus.models import Ogrenci, OgrenciProgram, OgrenciDersi
+from pyoko.exceptions import ObjectDoesNotExist
+
+
+class OgrenciHelper():
+    """OgrenciHelper Class
+
+    """
+
+    def diploma_notu_uret(self, ogrenci_no):
+        try:
+            ogrenci_program = OgrenciProgram.objects.get(ogrenci_no=ogrenci_no)
+            return ("%s-%s-%s" % (ogrenci_program.giris_tarihi,
+                                  ogrenci_program.program.yoksis_no, ogrenci_program.ogrenci_no))
+        except ObjectDoesNotExist:
+            return "Öğrenci Bulunamadı"
