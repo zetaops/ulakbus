2b16ecbaa29594682c38d50cc651c4a71033a58a
Mucahit Buyukyilmaz <mucahit@konya.edu.tr>
2016-04-06 11:34:27 +0300

    prepare_choices_for_model lib altına taşındı, referanslar düzeltildi rref #5250

--- ulakbus/lib/view_helpers.py
+++ ulakbus/lib/view_helpers.py
@@ -37,4 +37,8 @@ def prepare_titlemap_for_model(model, **kwargs):
 
     """
 
+<<<<<<< a9760f6370eb487f3300775585100fc3a42ba0db
     return [(m.key, m.__unicode__()) for m in model.objects.filter(**kwargs)]
+=======
+    return [{"value": m.key, "name": m.__unicode__()} for m in model.objects.filter(**kwargs)]
+>>>>>>> prepare_choices_for_model lib altına taşındı, referanslar düzeltildi
