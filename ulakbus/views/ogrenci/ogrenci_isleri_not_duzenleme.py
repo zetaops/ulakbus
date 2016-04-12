2014c7541efe13597d857413dbe351062b2c3952
yetercatikkas <yetercatikkas@gmail.com>
2016-04-12 12:06:37 +0300

    ADD: Ogrenci Isleri Not Duzenleme WF diagram ve views. rref #5270 GH-227

---
+++ ulakbus/views/ogrenci/ogrenci_isleri_not_duzenleme.py
@@ -0,0 +1,63 @@
+# -*-  coding: utf-8 -*-
+"""
+"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+from ulakbus.models import OgrenciProgram, OgrenciDersi, Sinav, DegerlendirmeNot
+from ulakbus.views.ders.ders import prepare_choices_for_model
+from zengine import forms
+from zengine.forms import fields
+from zengine.views.crud import CrudView
+
+
+class NotDuzenlemeForm(forms.JsonForm):
+    class Meta:
+        include = ['puan']
+
+    kaydet = fields.Button('Kaydet')
+
+
+class NotDuzenleme(CrudView):
+    class Meta:
+        model = 'DegerlendirmeNot'
+
+    def program_sec(self):
+        self.current.task_data['ogrenci_id'] = self.current.input['id']
+        _form = forms.JsonForm(current=self.current, title='Program Seçiniz.')
+        _choices = prepare_choices_for_model(OgrenciProgram, ogrenci_id=self.current.task_data['ogrenci_id'])
+        _form.program = fields.Integer(choices=_choices)
+        _form.onayla = fields.Button('Seç')
+        self.form_out(_form)
+
+    def ders_sec(self):
+        program_id = self.current.input['form']['program']
+        self.current.task_data['program_id'] = program_id
+        _form = forms.JsonForm(current=self.current, title='Ders Seçiniz.')
+        _choices = prepare_choices_for_model(OgrenciDersi, ogrenci_program_id=program_id,
+                                             ogrenci_id=self.current.task_data['ogrenci_id'])
+        _form.ders = fields.Integer(choices=_choices)
+        _form.onayla = fields.Button('Seç')
+        self.form_out(_form)
+
+    def sinav_sec(self):
+        ders_id = self.current.input['form']['ders']
+        self.current.task_data['ders_id'] = ders_id
+        ders = OgrenciDersi.objects.get(ders_id)
+        _form = forms.JsonForm(current=self.current, title='Sınav Seçiniz')
+        _choices = prepare_choices_for_model(Sinav, sube_id=ders.ders.key)
+        _form.sinav = fields.Integer(choices=_choices)
+        _form.onayla = fields.Button('Kaydet')
+        self.form_out(_form)
+
+    def not_duzenle(self):
+        sinav_id = self.current.input['form']['sinav']
+        # ders_id = self.current.task_data['ders_id']
+        # ogrenci_id = self.current.task_data['ogrenci_id']
+        degerlendirme_not = DegerlendirmeNot.objects.filter(sinav_id=sinav_id)[0]
+        title = '%s adlı öğrencinin % sınava ait notunu düzenleyiniz.' % (
+            degerlendirme_not.ogrenci, degerlendirme_not.sinav)
+        _form = NotDuzenlemeForm(degerlendirme_not, current=self.current, title=title)
+        self.form_out(_form)
