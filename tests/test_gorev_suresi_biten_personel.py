07816fd8170981ac6a139393801cd11fb050fe51
Ali Riza Keles <ali.r.keles@gmail.com>
2016-05-05 17:25:54 +0300

    FIX please do not use tab

--- tests/test_gorev_suresi_biten_personel.py
+++ tests/test_gorev_suresi_biten_personel.py
@@ -13,17 +13,18 @@ from zengine.lib.test_utils import BaseTestCase
 from ulakbus.models import User, Atama
 import datetime
 
+
 class TestCase(BaseTestCase):
-	def test_gorev_suresi_biten_personel(self):
-		""" 
-			Görev süresi dolan personel raporu sorgulanır.
-		"""
-		user = User.objects.get(username="mithat")
-		self.prepare_client("generic_reporter", user=user)
-		res = self.client.post(model="GorevSuresiBitenPersonel")
-		simdi = datetime.date.today()
-		atamalar = Atama.objects.filter(
-			gorev_suresi_bitis__lte=simdi,
-			personel_tip = 1
-			)
-		assert len(res.json["object"]["fields"]) == len(atamalar)
+    def test_gorev_suresi_biten_personel(self):
+        """
+            Görev süresi dolan personel raporu sorgulanır.
+        """
+        user = User.objects.get(username="mithat")
+        self.prepare_client("generic_reporter", user=user)
+        res = self.client.post(model="GorevSuresiBitenPersonel")
+        simdi = datetime.date.today()
+        atamalar = Atama.objects.filter(
+            gorev_suresi_bitis__lte=simdi,
+            personel_turu=1
+        )
+        assert len(res.json["object"]["fields"]) == len(atamalar)
