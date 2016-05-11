2703d111fd350dddee8f605c09ae05adeae35ac3
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-05-11 17:31:58 +0300

    ogrenci_mezuniyet wf testi ve datasi base.csv degişikliğine göre düzenlendi. rref #5317, p80 #5317

--- tests/test_ogrenci_mezuniyet.py
+++ tests/test_ogrenci_mezuniyet.py
@@ -44,7 +44,7 @@ class TestCase(BaseTestCase):
         program_id = "7GPhFaFbPqysh7mnkkd9Bq3cmCh"
 
         # veritabanından test_user seçilir
-        usr = User.objects.get(username='test_user')
+        usr = User.objects.get(username='personel_isleri_1')
         ogrenci = Ogrenci.objects.get(ogrenci_id)
         ogrenci_program = OgrenciProgram.objects.get(ogrenci=ogrenci)
         time.sleep(1)
