8bcbeaa240749dfaec81cffd11995787be68b5be
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-05-30 10:58:40 +0300

    ADD,yeni personel datasina gore terfisi_gelen_personel testi yazildi. rref #5301

--- tests/test_terfisi_gelen_personel.py
+++ tests/test_terfisi_gelen_personel.py
@@ -10,9 +10,12 @@
 
 from ulakbus.models import User, Personel
 from zengine.lib.test_utils import BaseTestCase
+from zengine.notifications.model import NotificationMessage
+import time
 
 
 class TestCase(BaseTestCase):
+    terfi_red = True
 
     def test_terfisi_gelen_personel(self):
 
@@ -25,14 +28,50 @@ class TestCase(BaseTestCase):
                        'personel_turu': 2}
         resp = self.client.post(form=idari_forms)
         ter_gel_id_per = resp.json['forms']['model']['Personel']
-        assert len(ter_gel_id_per) == 13
+        assert len(ter_gel_id_per) == 3
 
         idari_per_db = len(Personel.objects.filter(personel_turu=2))
 
-        assert idari_per_db - len(ter_gel_id_per) == 7
+        assert idari_per_db - len(ter_gel_id_per) == 17
 
-        ter_gel_id_per[1]['sec'] = True
-        ter_gel_id_per[5]['sec'] = True
+        # Terfisi yapilacak personel onaya gonder
+        resp = self.client.post(cmd='onaya_gonder', form={'Personel': ter_gel_id_per,
+                                                          'kaydet': 1})
+
+        assert resp.json['msgbox']['title'] == "Personeller Onay Icin Gonderildi!"
+        self.client.set_path('/logout')
+
+        time.sleep(1)
+
+        # Personel terfi islemini reddet bolumu
+        usr = User.objects.get(username='genel_sekreter_1')
+        msg = NotificationMessage.objects.filter(receiver=usr)[0]
+        token = msg.url.split('/')[-1]
+        self.prepare_client('/terfisi_gelen_personel_listesi', user=usr, token=token)
+        resp = self.client.post()
+        self.client.post(cmd="red_aciklamasi_yaz", form={'Personel': resp.json['forms']['model']['Personel'],
+                                                         'duzenle': 1})
+
+        resp = self.client.post(wf='terfisi_gelen_personel_listesi', form={'devam': 1,
+                                                                           'red_aciklama': "Reddedildi"})
+
+        assert resp.json['msgbox']['msg'] == "Talebiniz Basariyla iletildi."
+        self.client.set_path('/logout')
+
+        time.sleep(1)
+
+        usr = User.objects.get(username='personel_isleri_1')
+        msg = NotificationMessage.objects.filter(receiver=usr)[0]
+        token = msg.url.split('/')[-1]
+        self.prepare_client('/terfisi_gelen_personel_listesi', user=usr, token=token)
+        resp = self.client.post()
+        ter_gel_id_per = resp.json['forms']['model']['Personel']
+
+        assert len(ter_gel_id_per) == 3
+
+        # Terfisi yapilacak personel duzenle
+        ter_gel_id_per[0]['sec'] = True
+        ter_gel_id_per[2]['sec'] = True
 
         terfi_duz_forms = {'Personel': ter_gel_id_per,
                            'duzenle': 1}
@@ -45,21 +84,54 @@ class TestCase(BaseTestCase):
         ter_gel_id_per = resp.json['forms']['model']['Personel']
 
         resp = self.client.post(cmd='kaydet', form={'Personel': ter_gel_id_per,
-                                             'devam': 1})
+                                                    'devam': 1})
 
         ter_gel_id_per = resp.json['forms']['model']['Personel']
 
-        assert ter_gel_id_per[1]['yeni_gorev_ayligi'] == '2/7(7)' and\
-               ter_gel_id_per[5]['yeni_kazanilmis_hak'] == '3/5(5)'
+        assert ter_gel_id_per[0]['yeni_gorev_ayligi'] == '2/8(8)' and \
+               ter_gel_id_per[2]['yeni_kazanilmis_hak'] == '3/8(8)'
 
+        # Terfisi yapilacak personel onaya gonder
         resp = self.client.post(cmd='onaya_gonder', form={'Personel': ter_gel_id_per,
                                                           'kaydet': 1})
 
         assert resp.json['msgbox']['title'] == "Personeller Onay Icin Gonderildi!"
         self.client.set_path('/logout')
 
+        time.sleep(1)
+
+        self.personel_terfi_onay()
+
+    def personel_terfi_onay(self):
+        # Personel terfi islemini onayla bolumu
         usr = User.objects.get(username='genel_sekreter_1')
-        self.prepare_client('/terfisi_gelen_personel_listesi', user=usr)
+        msg = NotificationMessage.objects.filter(receiver=usr)[0]
+        token = msg.url.split('/')[-1]
+        self.prepare_client('/terfisi_gelen_personel_listesi', user=usr, token=token)
         resp = self.client.post()
 
-        assert len(resp.json['forms']['model']['Personel']) == len(ter_gel_id_per)
+        assert len(resp.json['forms']['model']['Personel']) == 3
+
+        ter_gel_id_per = resp.json['forms']['model']['Personel']
+
+        resp = self.client.post(cmd='terfi_yap', form={'Personel': ter_gel_id_per,
+                                                       'kaydet': 1})
+
+        assert resp.json['msgbox']['title'] == "Personel Terfi Islemi Onaylandi!"
+        self.client.set_path('/logout')
+
+        time.sleep(1)
+
+        usr = User.objects.get(username='personel_isleri_1')
+        msg = NotificationMessage.objects.filter(receiver=usr)[0]
+        token = msg.url.split('/')[-1]
+        self.prepare_client('/terfisi_gelen_personel_listesi', user=usr, token=token)
+        resp = self.client.post()
+
+        assert resp.json['msgbox']['title'] == "Terfi İşlemleri Onay Belgesi!"
+    #
+    # def get_notification(self, username):
+    #
+    #
+    #     return usr, token
+    #
--- ulakbus/views/personel/kadro.py
+++ ulakbus/views/personel/kadro.py
@@ -342,7 +342,7 @@ class TerfiForm(JsonForm):
             )
 
 
-class   TerfiDuzenleForm(JsonForm):
+class TerfiDuzenleForm(JsonForm):
     class Meta:
         inline_edit = [
             'yeni_gorev_ayligi_derece', 'yeni_gorev_ayligi_kademe', 'yeni_gorev_ayligi_gorunen',
@@ -560,6 +560,8 @@ class TerfiListe(CrudView):
     def taraflari_bilgilendir(self):
         msg = {"title": 'Personel Terfi Islemi Onaylandi!',
                "body": 'Onay Belgesi icin Personel Islerine Gonderildi.'}
+        self.current.output['msgbox'] = msg
+        self.current.task_data['LANE_CHANGE_MSG'] = msg
 
     def onay_belgesi_uret(self):
         self.current.output['msgbox'] = {
