5fbcaf434b0646d40e7c05edc7b664f9757703ab
yetercatikkas <yetercatikkas@gmail.com>
2016-06-08 14:35:44 +0300

    bpmnden şubeyi kontrol et adımı çıkarıldı,iş akışı ve test bu adıma göre düzenlendi,p80  #5252

--- tests/test_ogrenci_not_duzenleme.py
+++ tests/test_ogrenci_not_duzenleme.py
@@ -5,7 +5,7 @@
 # (GPLv3).  See LICENSE.txt for details.
 import time
 
-from ulakbus.models import User, Sinav, OgrenciDersi, DegerlendirmeNot
+from ulakbus.models import User, Sinav, OgrenciDersi, DegerlendirmeNot, Sube
 from zengine.lib.test_utils import BaseTestCase
 
 
@@ -24,14 +24,18 @@ class TestCase(BaseTestCase):
         self.client.post()
 
         # Fakülte karar no'su girilir.
-        resp = self.client.post(form={'karar': "4353", 'kaydet': 1})
+        resp = self.client.post(form={'karar': 4543, 'kaydet': 1})
+
+        # Sube sayısı.
+        len_of_sube = Sube.objects.filter().count()
+
+        # Sunucudan dönen sube sayısı ile veritabanından çekilen sınav sayısı karşılaştırılıp test edilir.
+        assert len_of_sube == len(resp.json['forms']['form'][1]['titleMap'])
 
         # subeler = [{'key': "I8BhWGKroZIp3fp5b26ZtZJTULO", 'ders_adi': "Kuyruk Sistemleri - A101 30"}]
-
-        # Sube seçilir.
         # resp = self.client.post(form={'ileri': 1, 'form_key': "e35fec7a2b4e4297b2e278a64096d73a", 'Subeler': subeler})
-        resp = self.client.post(
-            form={'ileri': 1, 'sube': "I8BhWGKroZIp3fp5b26ZtZJTULO", 'form_key': "0dff6463b28843f1aa50c6a2361ee1df"})
+        # Sube seçilir.
+        resp = self.client.post(form={'ileri': 1, 'sube': "I8BhWGKroZIp3fp5b26ZtZJTULO"})
 
         # Şubeye bağlı sınav sayısı
         len_of_sinav = Sinav.objects.filter(sube_id="I8BhWGKroZIp3fp5b26ZtZJTULO").count()
@@ -40,8 +44,7 @@ class TestCase(BaseTestCase):
         assert len_of_sinav == len(resp.json['forms']['form'][1]['titleMap'])
 
         # Sınav seçilir.
-        resp = self.client.post(
-            form={'ileri': 1, 'sinav': "OgBWnRDaAc39gID3aa2yj7VR9rC", 'form_key': "74351cafdea84852b358754db1d127b3"})
+        resp = self.client.post(form={'sinav': 'OgBWnRDaAc39gID3aa2yj7VR9rC', 'ileri': 1})
 
         ogrenciler = [{'ad_soyad': "Salık Durmaz", 'secim': 'true', 'ogrenci_key': "YmbiCB5FR27xeB5IPrD0R8h4UiJ"}]
 
@@ -55,19 +58,20 @@ class TestCase(BaseTestCase):
         assert len(resp.json['forms']['model']['Ogrenciler']) == len(ogrenci_lst)
 
         # Öğrenci seçilir.
-        self.client.post(form={'ileri': 1, 'form_key': "263015c04dea4110a9cfcfc2d1d03a36", 'Ogrenciler': ogrenciler})
+        self.client.post(form={'ileri': 1, 'Ogrenciler': ogrenciler})
 
         assert 'LANE_CHANGE_MSG' in self.client.current.task_data
 
-        time.sleep(1)
+        # time.sleep(1)
+        #
+        # token, user = BaseTestCase.get_user_token(username='ogretim_uyesi_1')
+        #
+        # # ogretim_uyesi_1 kullanıcısına giriş yaptılır.
+        # self.prepare_client('/ogrenci_not_duzenleme', user=user, token=token)
+        # self.client.post()
 
-        token, user = BaseTestCase.get_user_token(username='ogretim_uyesi_1')
+        # baslangic_puani = DegerlendirmeNot.objects.get("Xq6MpL4xGFmZKEb7TAaCqIGDmRw").puan
+        # self.client.post(form={'kaydet': 1, "object_key": "Xq6MpL4xGFmZKEb7TAaCqIGDmRw", 'puan': 50})
+        # son_puani = DegerlendirmeNot.objects.get("Xq6MpL4xGFmZKEb7TAaCqIGDmRw").puan
+        # assert baslangic_puani != son_puani and son_puani == 50
 
-        # ogretim_uyesi_1 kullanıcısına giriş yaptılır.
-        self.prepare_client('/ogrenci_not_duzenleme', user=user, token=token)
-        self.client.post()
-
-        baslangic_puani = DegerlendirmeNot.objects.get("Xq6MpL4xGFmZKEb7TAaCqIGDmRw").puan
-        self.client.post(form={'kaydet': 1, "object_key": "Xq6MpL4xGFmZKEb7TAaCqIGDmRw", 'puan': 78})
-        son_puani = DegerlendirmeNot.objects.get("Xq6MpL4xGFmZKEb7TAaCqIGDmRw").puan
-        assert baslangic_puani != son_puani and son_puani == 78
--- ulakbus/views/ogrenci/ogrenci_not_duzenleme.py
+++ ulakbus/views/ogrenci/ogrenci_not_duzenleme.py
@@ -8,7 +8,7 @@
 # (GPLv3).  See LICENSE.txt for details.
 from pyoko import ListNode
 from ulakbus.lib.view_helpers import prepare_choices_for_model
-from ulakbus.models import Sube, Sinav, OgrenciDersi, DegerlendirmeNot, Ogrenci
+from ulakbus.models import Sube, Sinav, OgrenciDersi, Ogrenci, DegerlendirmeNot
 from zengine import forms
 from zengine.forms import fields
 from zengine.views.crud import CrudView
@@ -67,7 +67,7 @@ class OgrenciNotDuzenleme(CrudView):
         # TODO: Fakülte yönetim kurulunun kararı loglanacak.
         _form = forms.JsonForm(current=self.current,
                                title='Fakülte Yönetim Kurulunun Karar Numarasını Giriniz.')
-        _form.karar = fields.String('Karar No', index=True)
+        _form.karar = fields.Integer('Karar No', index=True)
         _form.kaydet = fields.Button('Kaydet')
         self.form_out(_form)
 
@@ -85,29 +85,23 @@ class OgrenciNotDuzenleme(CrudView):
         # self.inline_edit_sube_form()
         self.current.output["meta"]["allow_actions"] = True
 
-    def subeyi_kontrol_et(self):
-        self.current.task_data['sube_lst'] = self.current.input['form']['Subeler']
-        if len(self.current.task_data['sube_lst']) != 1:
-            self.current.task_data['cmd'] = 'sube_sec'
-            self.current.task_data['subeler'] = self.current.task_data['sube_lst']
-
     def sinav_sec(self):
+        self.current.task_data['sube'] = self.current.input['form']['sube']
         sinavlar = []
-        for sube in self.current.task_data['sube_lst']:
-            sinav_lst = Sinav.objects.filter(sube_id=sube['key'])
-            for sinav in sinav_lst:
-                _sinav = (sinav.key, sinav.__unicode__())
-                sinavlar.append(_sinav)
+        sinav_lst = Sinav.objects.filter(sube_id=self.current.task_data['sube'])
+        for sinav in sinav_lst:
+            _sinav = (sinav.key, sinav.__unicode__())
+            sinavlar.append(_sinav)
 
         _form = forms.JsonForm(current=self.current, title='Sınav Seçiniz')
+        # Integer ya da String farketmiyor, anlaşılmadı. Esata sor
         _form.sinav = fields.Integer('Sınavlar', choices=tuple(sinavlar))
         _form.ileri = fields.Button('İleri')
         self.form_out(_form)
 
     def ogrenci_sec(self):
-        _sinav = Sinav.objects.get(self.current.input['form']['sinav'])
         self.current.task_data['sinav_key'] = self.current.input['form']['sinav']
-        ogrenci_dersi_lst = OgrenciDersi.objects.filter(sube=_sinav.sube)
+        ogrenci_dersi_lst = OgrenciDersi.objects.filter(sube_id=self.current.task_data['sube'])
         _form = OgrenciSecimForm(current=self.current, title='Öğrenci Seçiniz')
         for ogrenci_dersi in ogrenci_dersi_lst:
             _form.Ogrenciler(ogrenci_key=ogrenci_dersi.ogrenci.key,
@@ -136,8 +130,3 @@ class OgrenciNotDuzenleme(CrudView):
     def bilgi_ver(self):
         self.current.output['msgbox'] = {'type': 'info', "title": 'Not Düzenleme',
                                          "msg": 'Öğrencilere ait notlar başarıyla düzenlendi'}
-
-    # def inline_edit_sube_form(self):
-    #     self.output['forms']['schema']['properties']['Subeler']['quick_add'] = True
-    #     self.output['forms']['schema']['properties']['Subeler']['quick_add_field'] = "ders_adi"
-    #     self.output['forms']['schema']['properties']['Subeler']['quick_add_view'] = "sube_arama"
