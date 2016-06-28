0ad48ce8f7e003496fd7d1b89cfa2114b3b30227
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-06-28 10:58:44 +0300

    ADD, dokumantasyon eklendi. rref #5359

--- ulakbus/models/ders_programi.py
+++ ulakbus/models/ders_programi.py
@@ -13,6 +13,7 @@ Bu modül Ulakbüs uygulaması için öğrenci modeli ve öğrenciyle ilişkili 
 from pyoko import Model, field
 from .auth import Unit
 from .ogrenci import Okutman
+from ulakbus.lib.exceptions import DataConflictError
 
 UYGUNLUK_DURUMU = [
     (1, "Uygun"),
@@ -36,13 +37,25 @@ HAFTA_SONU_GUNLER = [
 HAFTA = HAFTA_ICI_GUNLER + HAFTA_SONU_GUNLER
 
 
-class DataConflictError(Exception):
-    pass
-
-
 class GunZamanDilimleri(Model):
+    """
+        Belirtilen birimin, haftanin secilen bir gunune ait saat araligi belirler.
+
+        Ornek:
+
+            birim - Bilgisayar Muhendisligi Bolumu (Unit Nesnesi)
+            gun - 1(Pazartesi)
+            baslama_saat = '10'
+            baslama_dakika = '30'
+            bitis_saat = '12'
+            bitis_dakika = '00'
+
+        Bu ornege gore Bilgisayar Muhendisligi Bolumunde pazartesi gunu 10:30-12:00 saatleri arasi
+        ders verilebilir. Bu belirlenen gune ait saat araliklari benzer olmamalidir. Eger cakisan
+        saat veye saat araligi olursa ilgili model DataConflictError hatasi dondurur.
+    """
     class Meta:
-        unique_together = [('birim', 'gun', 'baslama_saat', 'bitis_saat')]
+        unique_together = [('birim', 'gun', 'baslama_saat', 'baslama_dakika', 'bitis_saat', 'bitis_dakika')]
         search_fields = ['birim', 'gun', 'baslama_saat', 'bitis_saat']
 
     birim = Unit("Birim")
@@ -69,24 +82,23 @@ class GunZamanDilimleri(Model):
                 raise DataConflictError("Baslangic zamani bitis zamanindan buyuk olamaz.")
 
             if not ((yeni_baslangic < baslangic and yeni_bitis <= baslangic) or
-                        (yeni_baslangic >= bitis and yeni_bitis > bitis)):
+                    (yeni_baslangic >= bitis and yeni_bitis > bitis)):
                 raise DataConflictError("Baslangic bitis zamanlari onceki kayitlar ile cakismaktadir.")
 
     def __unicode__(self):
-        return '%s - %s' % (self.birim, self.gun)
+        return '%s - %s:%s' % (self.gun, self.baslama_saat, self.baslama_dakika)
 
 
 class OgElemaniZamanPlani(Model):
     """
-
-        pass
-
+        Okutman, birim ve okutmanin ilgili birimde verecegi  haftalik ders saati bilgisi tutulur.
     """
 
     class Meta:
         verbose_name = 'Ogretim Elemani Zaman Kaydi'
         verbose_name_plural = 'Ogretim Elemanlari Zaman Kaydi'
         unique_together = [('okutman', 'birim')]
+        search_fields = ['okutman', 'birim']
 
     okutman = Okutman("Okutman")
     birim = Unit("Birim")
@@ -97,9 +109,19 @@ class OgElemaniZamanPlani(Model):
 
 
 class ZamanCetveli(Model):
+    """
+        Ilgili birime ait belirlenen zaman dilimleri ders program koordinatoru tarafindan
+        ogretim elemanlarin saat araliklarina gore durumlarini belirleyecegi model
+    """
     class Meta:
+        verbose_name = 'Zaman Cetveli'
         unique_together = [('zaman_dilimi', 'ogretim_elemani_zaman_plani')]
+        search_fields = ['zaman_dilimi', 'ogretim_elemani_zaman_plani', 'birim']
 
+    birim = Unit("Birim")
     zaman_dilimi = GunZamanDilimleri("Zaman Dilimi")
-    durum = field.Integer("Uygunluk Durumu", choices=UYGUNLUK_DURUMU)
+    durum = field.Integer("Uygunluk Durumu", choices=UYGUNLUK_DURUMU, default=1, index=True)
     ogretim_elemani_zaman_plani = OgElemaniZamanPlani("Ogretim Elemani")
+
+    def __unicode__(self):
+        return '%s - %s - %s' % (self.ogretim_elemani_zaman_plani, self.zaman_dilimi, self.durum)
