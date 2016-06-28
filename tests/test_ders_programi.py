14267094ea20b11ed16f862639d6c16c248a012e
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-06-28 10:59:35 +0300

    ADD, ders programi modeline ait test yazildi. rref #5359

--- tests/test_ders_programi.py
+++ tests/test_ders_programi.py
@@ -11,38 +11,45 @@ import pytest
 
 
 class TestCase(BaseTestCase):
-
+    """
+        Test GunZamanDilimleri modelinde, ayni gune sahip baslangic saat-dakika ve
+        bitis saat-dakika zamanlarini kontrol eder. ilk basta cakismayan bir kayit girilir.
+        sonraki girilen zaman dilimlerinin bu kayit ile cakismasi beklenir. En sonda ise
+        girilen test datalari silinir.
+    """
     def test_ders_programi(self):
         unit = Unit.objects.get('EGGB7oyHWn8gVJnOlvqhzy1YCVV')
         zaman = GunZamanDilimleri()
         zaman.birim = unit
-        zaman.gun = 1
+        zaman.gun = 7
         zaman.baslama_saat = '09'
         zaman.baslama_dakika = '12'
         zaman.bitis_saat = '11'
         zaman.bitis_dakika = '45'
-        zaman.save()
+        zaman.blocking_save()
         assert zaman.just_created
 
-        with pytest.raises(DataConflictError):
+        with pytest.raises(DataConflictError) as exc:
             zaman1 = GunZamanDilimleri()
             zaman1.birim = unit
-            zaman1.gun = 1
+            zaman1.gun = 7
             zaman1.baslama_saat = '14'
             zaman1.baslama_dakika = '00'
             zaman1.bitis_saat = '12'
             zaman1.bitis_dakika = '30'
-            zaman1.save()
+            zaman1.blocking_save()
+        assert str(exc.value) in "Baslangic zamani bitis zamanindan buyuk olamaz."
 
-        with pytest.raises(DataConflictError):
+        with pytest.raises(DataConflictError) as exc:
             zaman2 = GunZamanDilimleri()
             zaman2.birim = unit
-            zaman2.gun = 1
+            zaman2.gun = 7
             zaman2.baslama_saat = '07'
             zaman2.baslama_dakika = '00'
             zaman2.bitis_saat = '09'
             zaman2.bitis_dakika = '30'
-            zaman2.save()
+            zaman2.blocking_save()
+        assert str(exc.value) in "Baslangic bitis zamanlari onceki kayitlar ile cakismaktadir."
 
         zaman.delete()
         zaman1.delete()
