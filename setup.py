0314ae14541691bf4e0f39d89b0378882549e72d
Ali Riza Keles <ali.r.keles@gmail.com>
2016-07-18 18:31:55 +0300

    FIX setup.py

--- setup.py
+++ setup.py
@@ -4,28 +4,26 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-        name='Ulakbus',
-        version='0.0.1',
-        description='Ulakbus Butunlesik Universite Sistemi',
-        author='Zetaops',
-        install_requires=[
-            'pyoko==0.7',
-            'zengine==0.7',
-            'passlib',
-            'Werkzeug',
-            'requests',
-            'boto',
-            'reportlab',
-            'six'
-        ],
-        dependency_links=[
-            'git+https://github.com/zetaops/pyoko.git@0.7#egg=pyoko',
-            'git+https://github.com/zetaops/zengine.git@0.7#egg=zengine'],
+    name='Ulakbus',
+    version='0.0.1',
+    description='Ulakbus Butunlesik Universite Sistemi',
+    author='Zetaops',
+    license='GPL v3',
+    author_email='info@zetaops.io',
+    install_requires=[
+        'zengine==0.7',
+        'requests',
+        'boto',
+        'reportlab',
+        'six'
+    ],
+    dependency_links=[
+        'git+https://github.com/zetaops/pyoko.git@0.7#egg=pyoko',
+        'git+https://github.com/zetaops/zengine.git@0.7#egg=zengine'],
 
-        author_email='info@zetaops.io',
-        url='https://github.com/zetaops/ulakbus',
-        packages=find_packages(exclude=['tests', 'tests.*']),
-        package_data={
-            'ulakbus': ['diagrams/*.bpmn'],
-        }
+    url='https://github.com/zetaops/ulakbus',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={
+        'ulakbus': ['diagrams/*.bpmn'],
+    }
 )
