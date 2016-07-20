ebadb4efc0e560b4f031225a1219d63f8527579a
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-07-20 14:40:26 +0300

    ADD, ogretim_elemani_zaman_tablosu ve derslik_zaman_tablosu bolum baskani adimi eklendi, testi yazildi.

--- tests/test_zaman_tablosu.py
+++ tests/test_zaman_tablosu.py
@@ -6,57 +6,149 @@
 
 from ulakbus.models import User, ZamanCetveli, DerslikZamanPlani
 from zengine.lib.test_utils import BaseTestCase
+import time
+from zengine.notifications.model import NotificationMessage
 
 
 class TestCase(BaseTestCase):
 
     def test_zaman_tablo(self):
+        for loop in range(2):
+            time.sleep(1)
+            if loop == 1:
+                token, user = self.get_user_token('ders_programi_koordinatoru_1')
+                self.prepare_client('/ogretim_elemani_zaman_tablosu', user=user, token=token)
+                resp = self.client.post()
+                assert resp.json['msgbox']['msg'] == "Reddedildi"
+                NotificationMessage.objects.filter().delete()
+            else:
+                usr = User.objects.get(username='ders_programi_koordinatoru_1')
+                self.prepare_client('/ogretim_elemani_zaman_tablosu', user=usr)
+                resp = self.client.post()
+                assert len(resp.json['ogretim_elemani_zt']['uygunluk_durumu']) == 15
 
-        usr = User.objects.get(username='ders_programi_koordinatoru_1')
-        self.prepare_client('/ogretim_elemani_zaman_tablosu', user=usr)
+            oe_change_key = 'AYgISJuQZftqTFSvkCv2lhDKvea'
+            oe_change_durum = 2
+
+            item_durum = {'key': oe_change_key,
+                          'durum': oe_change_durum}
+
+            self.client.post(cmd='degistir', change=item_durum)
+            zc = ZamanCetveli.objects.get(oe_change_key)
+
+            assert zc.durum == 2
+
+            zc.durum = 1
+            zc.save()
+
+            item_key = {'key': '5oumFDZlwPmjGmYpUPi7q7XBQVB'}
+            resp = self.client.post(cmd='personel_sec', secili_og_elemani=item_key)
+            assert resp.json['ogretim_elemani_zt']['oe_key'] == '5oumFDZlwPmjGmYpUPi7q7XBQVB'
+
+            for i in range(2):
+                resp = self.client.post(cmd='gonder')
+                assert resp.json['forms']['schema']['title'] == 'Ogretim Elemani Ders Programini Bolum Baskanina ' \
+                                                                'yollamak istiyor musunuz?'
+                if i == 0:
+                    resp = self.client.post(cmd='hayir')
+                    assert resp.json['ogretim_elemani_zt']['oe_key'] == '5oumFDZlwPmjGmYpUPi7q7XBQVB'
+                else:
+                    resp = self.client.post(cmd='evet')
+                    assert resp.json['msgbox']['title'] == 'Onay Icin Gonderildi!'
+
+            time.sleep(1)
+
+            token, user = self.get_user_token('bolum_baskani_2')
+            self.prepare_client('/ogretim_elemani_zaman_tablosu', user=user, token=token)
+            resp = self.client.post()
+            NotificationMessage.objects.filter().delete()
+            assert len(resp.json['ogretim_elemani_zt']['uygunluk_durumu']) == 15
+
+            resp = self.client.post(cmd='kontrol', secili_og_elemani=item_key)
+            assert resp.json['ogretim_elemani_zt']['oe_key'] == '5oumFDZlwPmjGmYpUPi7q7XBQVB'
+            if loop == 1:
+                resp = self.client.post(cmd='onay')
+                assert resp.json['msgbox']['title'] == 'Ogretim Elemani Zaman Tablosunu Onayladiniz!'
+            else:
+                self.client.post(cmd='reddet')
+                resp = self.client.post(form={"gonder": 1,
+                                              "mesaj": "Reddedildi"})
+                assert resp.json['msgbox']['title'] == "Red Aciklamasi Gonderildi!"
+        time.sleep(1)
+
+        token, user = self.get_user_token('ders_programi_koordinatoru_1')
+        self.prepare_client('/ogretim_elemani_zaman_tablosu', user=user, token=token)
         resp = self.client.post()
-        assert len(resp.json['ogretim_elemani_zt']['uygunluk_durumu']) == 15
-
-        oe_change_key = 'AYgISJuQZftqTFSvkCv2lhDKvea'
-        oe_change_durum = 2
-
-        item = {'key': oe_change_key,
-                'durum': oe_change_durum}
-
-        self.client.post(cmd='degistir', change=item)
-        zc = ZamanCetveli.objects.get(oe_change_key)
-
-        assert zc.durum == 2
-
-        zc.durum = 1
-        zc.save()
-
-        item = {'key': '5oumFDZlwPmjGmYpUPi7q7XBQVB'}
-        resp = self.client.post(cmd='personel_sec', secili_og_elemani=item)
-
-        assert resp.json['ogretim_elemani_zt']['oe_key'] == '5oumFDZlwPmjGmYpUPi7q7XBQVB'
+        assert resp.json['msgbox']['title'] == 'Talebiniz Onaylandi!'
+        NotificationMessage.objects.filter().delete()
 
     def test_derslik_zaman_tablosu(self):
-        usr = User.objects.get(username='ders_programi_koordinatoru_1')
-        self.prepare_client('/derslik_zaman_tablosu', user=usr)
+        for loop in range(2):
+            time.sleep(1)
+            if loop == 1:
+                token, user = self.get_user_token('ders_programi_koordinatoru_1')
+                self.prepare_client('/derslik_zaman_tablosu', user=user, token=token)
+                resp = self.client.post()
+                assert resp.json['msgbox']['msg'] == "Reddedildi"
+                NotificationMessage.objects.filter().delete()
+            else:
+                usr = User.objects.get(username='ders_programi_koordinatoru_1')
+                self.prepare_client('/derslik_zaman_tablosu', user=usr)
+                resp = self.client.post()
+                assert len(resp.json['derslik_zaman_tablosu']) == 4
+
+            derslik_key = '6Gj98J50zhRpqhjEQGlMxrL3SgF'
+            derslik_durum = 3
+
+            item_durum = {'key': derslik_key,
+                    'durum': derslik_durum}
+
+            self.client.post(cmd='kaydet', change=item_durum)
+
+            dz = DerslikZamanPlani.objects.get(derslik_key)
+            assert dz.derslik_durum == 3
+
+            dz.derslik_durum = 2
+            dz.save()
+
+            item_key = {'key': 'Rrm9587Fjzf5kMOtZBKn0Nl9UES'}
+            resp = self.client.post(cmd='derslik_degistir', secili_derslik=item_key)
+            assert resp.json['derslik_zaman_tablosu']['derslik_key'] == 'Rrm9587Fjzf5kMOtZBKn0Nl9UES'
+
+            for i in range(2):
+                resp = self.client.post(cmd='gonder')
+                assert resp.json['forms']['schema']['title'] == 'Derslik Ders Programini Bolum Baskanina ' \
+                                                                'yollaman istiyor musunuz?'
+                if i == 0:
+                    resp = self.client.post(cmd='hayir')
+                    assert resp.json['derslik_zaman_tablosu']['derslik_key'] == 'Rrm9587Fjzf5kMOtZBKn0Nl9UES'
+                else:
+                    resp = self.client.post(cmd='evet')
+                    assert resp.json['msgbox']['title'] == 'Onay Icin Gonderildi!'
+
+            time.sleep(1)
+
+            token, user = self.get_user_token('bolum_baskani_2')
+            self.prepare_client('/derslik_zaman_tablosu', user=user, token=token)
+            resp = self.client.post()
+            NotificationMessage.objects.filter().delete()
+            assert len(resp.json['derslik_zaman_tablosu']) == 4
+
+            resp = self.client.post(cmd='kontrol', secili_derslik=item_key)
+            assert resp.json['derslik_zaman_tablosu']['derslik_key'] == 'Rrm9587Fjzf5kMOtZBKn0Nl9UES'
+            if loop == 1:
+                resp = self.client.post(cmd='onay')
+                assert resp.json['msgbox']['title'] == 'Derslik Zaman Tablosunu Onayladiniz!'
+            else:
+                self.client.post(cmd='reddet')
+                resp = self.client.post(form={"gonder": 1,
+                                              "mesaj": "Reddedildi"})
+                assert resp.json['msgbox']['title'] == "Red Aciklamasi Gonderildi!"
+
+        time.sleep(1)
+
+        token, user = self.get_user_token('ders_programi_koordinatoru_1')
+        self.prepare_client('/derslik_zaman_tablosu', user=user, token=token)
         resp = self.client.post()
-        assert len(resp.json['derslik_zaman_tablosu']) == 4
-
-        derslik_key = '6Gj98J50zhRpqhjEQGlMxrL3SgF'
-        derslik_durum = 3
-
-        item = {'key': derslik_key,
-                'durum': derslik_durum}
-
-        self.client.post(cmd='kaydet', change=item)
-
-        dz = DerslikZamanPlani.objects.get(derslik_key)
-        assert dz.derslik_durum == 3
-
-        dz.derslik_durum = 2
-        dz.save()
-
-        item = {'key': 'Rrm9587Fjzf5kMOtZBKn0Nl9UES'}
-        resp = self.client.post(cmd='derslik_degistir', secili_derslik=item)
-
-        assert resp.json['derslik_zaman_tablosu']['derslik_key'] == 'Rrm9587Fjzf5kMOtZBKn0Nl9UES'
+        assert resp.json['msgbox']['title'] == 'Talebiniz Onaylandi!'
+        NotificationMessage.objects.filter().delete()
--- ulakbus/views/ders_programi/zaman_tablosu.py
+++ ulakbus/views/ders_programi/zaman_tablosu.py
@@ -9,6 +9,7 @@
 #
 
 from zengine.views.crud import CrudView
+from zengine.forms import JsonForm, fields
 from ulakbus.models.ders_programi import OgElemaniZamanPlani, ZamanCetveli, DerslikZamanPlani
 from ulakbus.models import Room
 
@@ -28,15 +29,110 @@ class ZamanTablo(CrudView):
         """
         # default select first person
         try:
-            if self.current.input['secili_og_elemani']['key']:
+            if self.current.task_data['cmd'] == 'personel_sec':
                 self.current.task_data['ogretim_elemani_key'] = self.current.input['secili_og_elemani']['key']
+            elif self.current.task_data['cmd'] == 'hayir':
+                pass
+            else:
+                self.current.task_data['ogretim_elemani_key'] = \
+                    (OgElemaniZamanPlani.objects.filter(birim=self.current.role.unit)[0]).key
         except:
             self.current.task_data['ogretim_elemani_key'] = \
                 (OgElemaniZamanPlani.objects.filter(birim=self.current.role.unit)[0]).key
 
     def zaman_tablosu_listele(self):
         """
-        Creates a message for the given channel.
+        .. code-block:: python
+            # response:
+            {
+                'ogretim_elemani': {
+                    'oe_key': string   # ogretim_elemani_key
+                    'name': string,     # name surname,
+                    'toplam_ders_saati': int,   # hours,
+                    'uygunluk_durumu': [{
+                        'key': string,     # zaman_cetveli_key
+                        'saat': string,  # 10:00-12:00,
+                        'gun': int,     # 1 = pazartesi,
+                        'durum': int    # 2 = mumkunse uygun degil,
+                        }]}
+            }
+        """
+        try:
+            if self.current.task_data['red_aciklamasi']:
+                self.current.output['msgbox'] = {"type": "warning",
+                                                 "title": "Talebiniz Bolum Baskani Tarafindan Reddedildi",
+                                                 "msg": self.current.task_data['red_aciklamasi']}
+        except KeyError:
+            pass
+
+        self.current.ogretim_elemani = OgElemaniZamanPlani.objects.get(self.current.task_data['ogretim_elemani_key'])
+        zaman_cetveli = sorted(ZamanCetveli.objects.filter(ogretim_elemani_zaman_plani=self.current.ogretim_elemani),
+                               key=lambda z: (z.zaman_dilimi, z.gun))
+
+        uygunluk_durumu = list()
+
+        for zc in zaman_cetveli:
+            durum = dict()
+            durum['key'] = zc.key
+            durum['saat'] = zc.zaman_dilimi.baslama_saat + ':' + zc.zaman_dilimi.baslama_dakika + '-' + \
+                            zc.zaman_dilimi.bitis_saat + ':' + zc.zaman_dilimi.bitis_dakika
+            durum['gun'] = zc.gun
+            durum['durum'] = zc.durum
+            uygunluk_durumu.append(durum)
+
+        item = {'oe_key': self.current.ogretim_elemani.key,
+                'name': self.current.ogretim_elemani.okutman.ad + ' ' + self.current.ogretim_elemani.okutman.soyad,
+                'toplam_ders_saati': self.current.ogretim_elemani.toplam_ders_saati,
+                'uygunluk_durumu': uygunluk_durumu}
+
+        self.output['ogretim_elemani_zt'] = item
+        _form = JsonForm()
+        _form.gonder = fields.Button('Onaya Gonder', cmd='onaya_gonder')
+        self.form_out(_form)
+
+    def zaman_degisiklik_kaydet(self):
+        """"
+
+        # request:
+        {
+            'change':{
+                'key': string   # key,
+                'durum': int    # 1 = uygun,
+                'cmd': 'degistir'}
+        }
+
+        """
+
+        change = self.current.input['change']
+        key = change['key']
+        zc = ZamanCetveli.objects.get(key)
+        zc.durum = change['durum']
+        zc.save()
+
+    def onaya_gonder(self):
+        _form = JsonForm(title='Ogretim Elemani Ders Programini Bolum Baskanina yollamak istiyor musunuz?')
+        _form.evet = fields.Button('Evet', cmd='evet')
+        _form.hayir = fields.Button('Hayir', cmd='hayir')
+        self.form_out(_form)
+
+    def mesaj(self):
+        msg = {"title": 'Onay Icin Gonderildi!',
+               "body": 'Talebiniz Basariyla iletildi.'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
+
+        self.current.task_data['LANE_CHANGE_MSG'] = msg
+
+    def bilgilendirme(self):
+        msg = {"type": "info",
+               "title": 'Talebiniz Onaylandi!',
+               "msg": 'Gonderdiginiz ogretim elemani zaman tablosu Bolum Baskani tarafindan onaylandi'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
+
+        self.current.output['msgbox'] = msg
+
+    # Lane Gecisi Bolum Baskani
+    def onay_ekrani(self):
+        """
         .. code-block:: python
             # response:
             {
@@ -73,46 +169,53 @@ class ZamanTablo(CrudView):
                 'uygunluk_durumu': uygunluk_durumu}
 
         self.output['ogretim_elemani_zt'] = item
+        _form = JsonForm()
+        _form.onayla = fields.Button('Onayla', cmd='onay')
+        _form.reddet = fields.Button('Reddet', cmd='reddet')
+        self.form_out(_form)
 
-    def zaman_degisiklik_kaydet(self):
-        """"
+    def kontrol_et(self):
+        """
 
         # request:
         {
-            'change':{
-                'key': string   # key,
-                'durum': int    # 1 = uygun,
-                'cmd': 'degistir'}
+            'secili_og_elemani':{
+                'key': string   #  personel key,
+                'cmd': 'kontrol'}
         }
 
         """
+        # default select first person
+        try:
+            if self.current.task_data['cmd'] == 'kontrol':
+                self.current.task_data['ogretim_elemani_key'] = self.current.input['secili_og_elemani']['key']
+            else:
+                self.current.task_data['ogretim_elemani_key'] = \
+                    (OgElemaniZamanPlani.objects.filter(birim=self.current.role.unit)[0]).key
+        except:
+            self.current.task_data['ogretim_elemani_key'] = \
+                (OgElemaniZamanPlani.objects.filter(birim=self.current.role.unit)[0]).key
 
-        change = self.current.input['change']
-        key = change['key']
-        zc = ZamanCetveli.objects.get(key)
-        zc.durum = change['durum']
-        zc.save()
+    def aciklama_yaz(self):
+        _form = JsonForm()
+        _form.mesaj = fields.String('Aciklama')
+        _form.gonder = fields.Button('gonder')
+        self.form_out(_form)
 
-    def onaya_gonder(self):
-        msg = {"title": 'Onay İçin Gönderildi!!',
-               "body": 'Ögretim elemanı zaman tablosu onay için Bölüm Başkanına gönderildi.'}
+    def red_aciklamasini_gonder(self):
+        self.current.task_data['red_aciklamasi'] = self.current.input['form']['mesaj']
+        msg = {"title": 'Red Aciklamasi Gonderildi!',
+               "body": 'Isleminiz basariyla gerceklesmistir'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
 
-        self.current.output['msgbox'] = msg
         self.current.task_data['LANE_CHANGE_MSG'] = msg
 
-    def bilgilendirme(self):
-        pass
-
-    # Lane Gecisi Bolum Baskani
-
-    def onay_ekrani(self):
-        pass
-
-    def reddet_ve_geri_gonder(self):
-        pass
-
     def onayla(self):
-        pass
+        msg = {"title": 'Ogretim Elemani Zaman Tablosunu Onayladiniz!',
+               "body": 'Bolum ders programi koordinatorune onaylama iletildi.'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
+
+        self.current.task_data['LANE_CHANGE_MSG'] = msg
 
 
 class DerslikZamanTablosu(CrudView):
@@ -129,15 +232,123 @@ class DerslikZamanTablosu(CrudView):
 
         """
         try:
-            if self.current.input['secili_derslik']['key']:
+            if self.current.task_data['cmd'] == 'derslik_degistir':
                 self.current.task_data['room_key'] = self.current.input['secili_derslik']['key']
+            elif self.current.task_data['cmd'] == 'hayir':
+                pass
+            else:
+                self.current.task_data['room_key'] = Room.objects.raw("room_departments.unit_id:" +
+                                                                      self.current.role.unit.key)[0].key
         except:
             self.current.task_data['room_key'] = Room.objects.raw("room_departments.unit_id:" +
                                                                   self.current.role.unit.key)[0].key
 
     def listele(self):
         """
-        Creates a message for the given channel.
+        .. code-block:: python
+            # response:
+            {
+                'derslik_zaman_tablosu': {
+                    'derslik_key': string   # derslik key
+                    'name': string,     # room name,
+                    'kapasite': int,   # capacity,
+                    'zaman_plani': [{
+                        'key': string,     # derslik zaman plani key
+                        'saat': string,  # 10:00-12:00,
+                        'gun': int,     # 1 = pazartesi,
+                        'durum': int    # 2 = Bolume Ait,
+                        }]}
+            }
+        """
+
+        try:
+            if self.current.task_data['red_aciklamasi']:
+                self.current.output['msgbox'] = {"type": "warning",
+                                                 "title": "Talebiniz Bolum Baskani Tarafindan Reddedildi",
+                                                 "msg": self.current.task_data['red_aciklamasi']}
+        except KeyError:
+            pass
+
+        self.current.room = Room.objects.get(self.current.task_data['room_key'])
+        dzps = sorted(DerslikZamanPlani.objects.filter(derslik=self.current.room),
+                      key=lambda d: (d.gun, d.baslangic_saat, d.baslangic_dakika))
+
+        zaman_plani = list()
+
+        for dz in dzps:
+            durum = dict()
+            durum['key'] = dz.key
+            durum['saat'] = dz.baslangic_saat + ':' + dz.baslangic_dakika + '-' + dz.bitis_saat + ':' + dz.bitis_dakika
+            durum['gun'] = dz.gun
+            durum['durum'] = dz.derslik_durum
+            zaman_plani.append(durum)
+
+        item = {'derslik_key': self.current.room.key,
+                'name': self.current.room.name,
+                'kapasite': self.current.room.capacity,
+                'zaman_plani': zaman_plani}
+
+        self.output['derslik_zaman_tablosu'] = item
+        _form = JsonForm()
+        _form.gonder = fields.Button('Onaya Gonder', cmd='onaya_gonder')
+        self.form_out(_form)
+
+    def degisiklikleri_kaydet(self):
+        """"
+
+        # request:
+        {
+            'change':{
+                'key': string   # key,
+                'durum': int    # 1 = Herkese Acik,
+                'cmd': 'kaydet'}
+        }
+
+        """
+        change = self.current.input['change']
+        key = change['key']
+        dz = DerslikZamanPlani.objects.get(key)
+        dz.derslik_durum = change['durum']
+        dz.save()
+
+    def onaya_gonder(self):
+        _form = JsonForm(title='Derslik Ders Programini Bolum Baskanina yollaman istiyor musunuz?')
+        _form.evet = fields.Button('Evet', cmd='evet')
+        _form.hayir = fields.Button('Hayir', cmd='hayir')
+        self.form_out(_form)
+
+    def mesaj(self):
+        msg = {"title": 'Onay Icin Gonderildi!',
+               "body": 'Talebiniz Basariyla iletildi.'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
+
+        self.current.task_data['LANE_CHANGE_MSG'] = msg
+
+    def kontrol_et(self):
+        """
+
+        # request:
+        {
+            'secili_derslik':{
+                'key': string   #  derslik key,
+                'cmd': 'kontrol'}
+        }
+
+        """
+        try:
+            if self.current.task_data['cmd'] == 'kontrol':
+                self.current.task_data['room_key'] = self.current.input['secili_derslik']['key']
+            elif self.current.task_data['cmd'] == 'hayir':
+                pass
+            else:
+                self.current.task_data['room_key'] = Room.objects.raw("room_departments.unit_id:" +
+                                                                      self.current.role.unit.key)[0].key
+        except:
+            self.current.task_data['room_key'] = Room.objects.raw("room_departments.unit_id:" +
+                                                                  self.current.role.unit.key)[0].key
+
+    def onay_ekrani(self):
+        """
         .. code-block:: python
             # response:
             {
@@ -173,40 +384,37 @@ class DerslikZamanTablosu(CrudView):
                 'zaman_plani': zaman_plani}
 
         self.output['derslik_zaman_tablosu'] = item
+        _form = JsonForm()
+        _form.onayla = fields.Button('Onayla', cmd='onay')
+        _form.reddet = fields.Button('Reddet', cmd='reddet')
+        self.form_out(_form)
 
-    def degisiklikleri_kaydet(self):
-        """"
+    def red_aciklama_yaz(self):
+        _form = JsonForm()
+        _form.mesaj = fields.String('Aciklama')
+        _form.gonder = fields.Button('gonder')
+        self.form_out(_form)
 
-        # request:
-        {
-            'change':{
-                'key': string   # key,
-                'durum': int    # 1 = Herkese Acik,
-                'cmd': 'kaydet'}
-        }
+    def geri_gonder(self):
+        self.current.task_data['red_aciklamasi'] = self.current.input['form']['mesaj']
+        msg = {"title": 'Red Aciklamasi Gonderildi!',
+               "body": 'Isleminiz basariyla gerceklesmistir'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
 
-        """
-        change = self.current.input['change']
-        key = change['key']
-        dz = DerslikZamanPlani.objects.get(key)
-        dz.derslik_durum = change['durum']
-        dz.save()
-
-    def onaya_gonder(self):
-        msg = {"title": 'Onay İçin Gönderildi!!',
-               "body": 'Derslik zaman tablosu onay için Bölüm Başkanına gönderildi.'}
-
-        self.current.output['msgbox'] = msg
         self.current.task_data['LANE_CHANGE_MSG'] = msg
 
-    def onay_ekrani(self):
-        pass
-
-    def reddet_ve_geri_gonder(self):
-        pass
-
     def onayla(self):
-        pass
+        msg = {"title": 'Derslik Zaman Tablosunu Onayladiniz!',
+               "body": 'Bolum ders programi koordinatorune onaylama iletildi.'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
+
+        self.current.task_data['LANE_CHANGE_MSG'] = msg
 
     def bilgilendirme(self):
-        pass
+        msg = {"type": "info",
+               "title": 'Talebiniz Onaylandi!',
+               "msg": 'Gonderdiginiz derslik zaman tablosu Bolum Baskani tarafindan onaylandi'}
+        # workflowun bu kullanıcı için bitişinde verilen mesajı ekrana bastırır
+
+        self.current.output['msgbox'] = msg
+
