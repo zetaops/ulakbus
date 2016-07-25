654e48eb9bcaf151a7004f21d674efbc9ebad657
Furkan Uyar <furkanuyar92@gmail.com>
2016-07-25 22:37:16 +0300

    Tekrarlanan methodlar kaldirildi. rref #5397

--- ulakbus/views/sinav_programi/ogrenci_sinav_programi_goruntule.py
+++ ulakbus/views/sinav_programi/ogrenci_sinav_programi_goruntule.py
@@ -8,20 +8,18 @@
 from zengine.forms import JsonForm, fields
 from zengine.views.crud import CrudView
 from collections import OrderedDict
-from ulakbus.models import Okutman, Personel, Sube, Donem, Ogrenci, OgrenciDersi
-from ulakbus.models.ders_programi_data import SinavEtkinligi
+from ulakbus.models import Donem, Ogrenci, OgrenciDersi
 from ulakbus.models.ders_programi import HAFTA
-# from ulakbus.views.sinav_programi import okutman_sinav_programi_goruntuleme as SP
-import calendar
+from ulakbus.views.sinav_programi import okutman_sinav_programi_goruntule as SP
+
 
 class Ogrenci_Sinav_Programi_Goruntule(CrudView):
-
     def sinav_programi_goruntule(self):
 
-        # ogrenci = Ogrenci.objects.get(user = self.current.user)
-        ogrenci = Ogrenci.objects.get('Fj3ysQn3lMlgXcxhGILzsfAiGZ')
-        guncel_donem = Donem.objects.get(guncel = True)
-        ogrenci_dersleri = OgrenciDersi.objects.filter(ogrenci = ogrenci,donem = guncel_donem)
+        ogrenci = Ogrenci.objects.get(user=self.current.user)
+        # ogrenci = Ogrenci.objects.get('Fj3ysQn3lMlgXcxhGILzsfAiGZ')
+        guncel_donem = Donem.objects.get(guncel=True)
+        ogrenci_dersleri = OgrenciDersi.objects.filter(ogrenci=ogrenci, donem=guncel_donem)
 
         subeler = []
         for ogrenci_ders in ogrenci_dersleri:
@@ -29,22 +27,23 @@ class Ogrenci_Sinav_Programi_Goruntule(CrudView):
 
         ogrenci_adi = ogrenci.ad + ' ' + ogrenci.soyad
 
-        sinav_etkinlik = sinav_etkinlik_olustur(subeler)
-        object_list = ['Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi','Pazar']
+        sinav_etkinlik = SP.sinav_etkinlik_olustur(subeler)
+        object_list = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar']
         self.output['objects'] = [object_list]
 
         _form = JsonForm(current=self.current)
         _form.title = "%s %s Sınav Programı" % (ogrenci_adi, guncel_donem.ad)
 
-        hafta_dict = hafta_gun_olustur(HAFTA)
-        for i in range(max(map(len,sinav_etkinlik.values()))):
+        hafta_dict = SP.hafta_gun_olustur(HAFTA)
+        for i in range(max(map(len, sinav_etkinlik.values()))):
             sinav_etkinlik_list = OrderedDict({})
             for hafta_gun in hafta_dict.keys():
                 if hafta_gun in sinav_etkinlik:
                     try:
                         etkinlik = sinav_etkinlik[hafta_gun][i]
-                        sinav_saat = "%02d" %etkinlik.tarih.time().hour +':' + "%02d" %etkinlik.tarih.time().minute
-                        sinav_etkinlik_list[hafta_dict[hafta_gun]] = etkinlik.sube.ad+' / '+ etkinlik.tarih.strftime('%d:%m:%Y')+ ' / ' + sinav_saat
+                        sinav_saat = "%02d" % etkinlik.tarih.time().hour + ':' + "%02d" % etkinlik.tarih.time().minute
+                        sinav_etkinlik_list[hafta_dict[hafta_gun]] = etkinlik.sube.ad + ' / ' + etkinlik.tarih.strftime(
+                            '%d:%m:%Y') + ' / ' + sinav_saat
                     except:
                         sinav_etkinlik_list[hafta_dict[hafta_gun]] = ''
 
@@ -56,39 +55,8 @@ class Ogrenci_Sinav_Programi_Goruntule(CrudView):
                 "fields": sinav_etkinlik_list,
                 "actions": False,
                 'key': ''
-                   }
+            }
 
             self.output['objects'].append(item)
 
         self.form_out(_form)
-
-def sinav_etkinlik_olustur(subeler):
-
-    sinav_etkinlik = {}
-    for sube in subeler:
-        try:
-            etkinlik = SinavEtkinligi.objects.get(sube=sube)
-            tarih = etkinlik.tarih
-            gun = calendar.weekday(tarih.year, tarih.month, tarih.day) + 1
-
-            if gun in sinav_etkinlik:
-                sinav_etkinlik[gun].append(etkinlik)
-            else:
-                sinav_etkinlik[gun] = [etkinlik]
-        except:
-            pass
-    return sinav_etkinlik
-
-def hafta_gun_olustur(HAFTA):
-
-    hafta_dict = {}
-    for i in range(len(HAFTA)):
-        hafta_dict[HAFTA[i][0]] = HAFTA[i][1]
-
-    return hafta_dict
-
-
-
-
-
-
