d7a3442fa41324f222d315a6dda78579a4c9b82e
Furkan Uyar <furkanuyar92@gmail.com>
2016-07-26 23:52:20 +0300

    ADD, Sol menü için ad değiştirildi ve dokümantasyon yapıldı. rref #5397

--- ulakbus/views/sinav_programi/ogrenci_sinav_programi_goruntule.py
+++ ulakbus/views/sinav_programi/ogrenci_sinav_programi_goruntule.py
@@ -5,10 +5,11 @@
 # (GPLv3).  See LICENSE.txt for details.
 #
 
-from zengine.forms import JsonForm, fields
+from zengine.forms import JsonForm
 from zengine.views.crud import CrudView
 from collections import OrderedDict
-from ulakbus.models import Donem, Ogrenci, OgrenciDersi
+from ulakbus.models import Donem, Ogrenci, OgrenciDersi, \
+    Sube  # eğer haftanın günü(1,2..) öğretim görevlisinin sınavı varsa
 from ulakbus.models.ders_programi import HAFTA
 from ulakbus.views.sinav_programi import okutman_sinav_programi_goruntule as SP
 
@@ -16,33 +17,49 @@ from ulakbus.views.sinav_programi import okutman_sinav_programi_goruntule as SP
 class Ogrenci_Sinav_Programi_Goruntule(CrudView):
     def sinav_programi_goruntule(self):
 
+        """
+        Öğrenci kendi şubelerine ait sinav
+        programını görüntüleyebilir.
+
+        """
         ogrenci = Ogrenci.objects.get(user=self.current.user)
-        # ogrenci = Ogrenci.objects.get('Fj3ysQn3lMlgXcxhGILzsfAiGZ')
         guncel_donem = Donem.objects.get(guncel=True)
+        # Güncel döneme ve giriş yapan, öğrenciye ait öğrenci_dersleri bulunur.
         ogrenci_dersleri = OgrenciDersi.objects.filter(ogrenci=ogrenci, donem=guncel_donem)
 
         subeler = []
+        # Bulunan öğrenci derslerinin şubeleri bulunur ve listeye eklenir.
         for ogrenci_ders in ogrenci_dersleri:
-            subeler.append(ogrenci_ders.sube)
+            try:
+                sube = Sube.objects.get(ogrenci_ders.sube.key)
+                subeler.append(sube)
+            except:
+                pass
 
         ogrenci_adi = ogrenci.ad + ' ' + ogrenci.soyad
 
+        # öğrencinin şubelerine göre sınav etkinlikleri
+        # dictionary'e eklenir.
         sinav_etkinlik = SP.sinav_etkinlik_olustur(subeler)
         object_list = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar']
         self.output['objects'] = [object_list]
 
         _form = JsonForm(current=self.current)
-        _form.title = "%s %s Sınav Programı" % (ogrenci_adi, guncel_donem.ad)
+        _form.title = "%s / %s Sınav Programı" % (ogrenci_adi, guncel_donem.ad)
 
         hafta_dict = SP.hafta_gun_olustur(HAFTA)
+        # Öğrencinin bir günde maksimum kaç tane sınavı olduğu bulunur
+        # ve bu bilgi kadar dönülür.
         for i in range(max(map(len, sinav_etkinlik.values()))):
             sinav_etkinlik_list = OrderedDict({})
+            # eğer haftanın günü(1,2..) öğrencinin sınavı varsa
             for hafta_gun in hafta_dict.keys():
                 if hafta_gun in sinav_etkinlik:
                     try:
                         etkinlik = sinav_etkinlik[hafta_gun][i]
-                        sinav_saat = "%02d" % etkinlik.tarih.time().hour + ':' + "%02d" % etkinlik.tarih.time().minute
-                        sinav_etkinlik_list[hafta_dict[hafta_gun]] = etkinlik.sube.ad + ' / ' + etkinlik.tarih.strftime(
+                        sinav_saat = "%02d:%02d" % (etkinlik.tarih.time().hour, etkinlik.tarih.time().minute)
+                        sinav_etkinlik_list[hafta_dict[hafta_gun]] = str(
+                            etkinlik.sube.ad) + ' / ' + etkinlik.tarih.strftime(
                             '%d:%m:%Y') + ' / ' + sinav_saat
                     except:
                         sinav_etkinlik_list[hafta_dict[hafta_gun]] = ''
