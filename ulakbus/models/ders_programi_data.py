37978605b54cf886db930a5b481b2fdf315caf3c
furkanUyar <furkanuyar92@gmail.com>
2016-08-04 10:00:02 +0300

    Öğretim görevlisi ve öğrenci için sınav etkinliği getir methodları yazıldı. Testleri yazıldı. rref #5398

--- ulakbus/models/ders_programi_data.py
+++ ulakbus/models/ders_programi_data.py
@@ -8,7 +8,6 @@ from pyoko import Model, ListNode
 from zengine.forms import fields
 from . import RoomType, Okutman, Room, Sube, Donem, Unit, Ders, HAFTA, OgrenciDersi
 
-
 class DersEtkinligi(Model):
 
     class Meta:
