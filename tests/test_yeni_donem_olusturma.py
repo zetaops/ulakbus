722e9ca7ef4d664bda2d4f02a6022045da6206ac
yetercatikkas <yetercatikkas@gmail.com>
2016-08-09 16:39:01 +0300

    test_yeni_donem_olusturma testinde oluşan data silinmesi için kod satırları eklendi

--- tests/test_yeni_donem_olusturma.py
+++ tests/test_yeni_donem_olusturma.py
@@ -32,7 +32,7 @@ class TestCase(BaseTestCase):
 
         # Yeni dönemler kaydedilmeden önceki dönem sayısı.
         baslangictaki_donem_sayisi = Donem.objects.filter().count()
-
+        onceki_donem_keyleri = [d.key for d in Donem.objects.filter()]
         # Güz ve Bahar dönemi formları doldurulur, request yapılır.
         resp = self.client.post(
             form={'kaydet': 1, 'guz_baslangic_tarihi': '02.09.2015', 'guz_bitis_tarihi': '15.02.2016',
@@ -47,3 +47,8 @@ class TestCase(BaseTestCase):
         assert son_donem_sayisi == baslangictaki_donem_sayisi + 2, 'Yeni eklenen dönemler kaydedilmedi.'
         # Dönen cevapta msgbox olup olmadığı test edilir.
         assert 'msgbox' in resp.json
+
+        sonraki_donem_keyleri = [d.key for d in Donem.objects.filter()]
+        silinen_donemler = [Donem.objects.get(d_key).delete() for d_key in sonraki_donem_keyleri if
+                            d_key not in onceki_donem_keyleri]
+        assert son_donem_sayisi - len(silinen_donemler) == baslangictaki_donem_sayisi
