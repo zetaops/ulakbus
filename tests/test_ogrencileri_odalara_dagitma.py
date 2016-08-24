09b3dfa3c0949b22baa507b6417349f69bf8c2c3
furkanUyar <furkanuyar92@gmail.com>
2016-08-24 16:35:44 +0300

    Test genişletildi.

--- tests/test_ogrencileri_odalara_dagitma.py
+++ tests/test_ogrencileri_odalara_dagitma.py
@@ -21,19 +21,24 @@ class TestCase(BaseTestCase):
 
         """
 
-        sinav_etkinligi = SinavEtkinligi.objects.filter()[0]
+        sinav_etkinligi = SinavEtkinligi.objects.get('XHTfo0wYYxkWSv9YNTlG0XddL1l')
+        # ogrenciler = sinav_etkinligi.sinav_ogrenci_listesi()
+        # for ogrenci in ogrenciler:
+        #     sinav_etkinligi.Ogrenciler(ogrenci = ogrenci)
+        #     sinav_etkinligi.save()
 
-        assert len(sinav_etkinligi.Ogrenciler) == OgrenciDersi.objects.filter(sube = sinav_etkinligi.sube,donem = sinav_etkinligi.donem).count()
+        assert len(sinav_etkinligi.Ogrenciler) == OgrenciDersi.objects.filter(sube = sinav_etkinligi.sube,
+                                                                              donem = sinav_etkinligi.donem).count()
 
         toplam_kontenjan = 0
         for sinav_yeri in sinav_etkinligi.SinavYerleri:
             toplam_kontenjan += sinav_yeri.room.capacity
 
-        assert toplam_kontenjan > len(sinav_etkinligi.Ogrenciler)
+        assert toplam_kontenjan >= len(sinav_etkinligi.Ogrenciler)
 
         oran = SinavEtkinligi.doluluk_orani_hesapla(sinav_etkinligi)
 
-        assert oran < 1
+        assert oran <= 1
 
         SinavEtkinligi.ogrencileri_odalara_dagit(sinav_etkinligi,oran)
 
@@ -41,10 +46,18 @@ class TestCase(BaseTestCase):
         for sinav_yeri in sinav_etkinligi.SinavYerleri:
             sinav_yerleri_listesi.append(sinav_yeri.room)
         sinav_yerleri = {}
+
         for etkinlik in sinav_etkinligi.Ogrenciler:
             assert etkinlik.room is not None
             assert etkinlik.room in sinav_yerleri_listesi
-            sinav_yerleri[etkinlik.room.key] += 1 if etkinlik.room.code in sinav_yerleri else 1
+
+            if etkinlik.room.code in sinav_yerleri:
+                sinav_yerleri[etkinlik.room.key] += 1
+            else:
+                sinav_yerleri[etkinlik.room.key] = 1
+
+
+            # sinav_yerleri[etkinlik.room.key] += 1 if etkinlik.room.code in sinav_yerleri else 1
 
         for yer in sinav_yerleri.keys():
-            assert sinav_yerleri[yer] <= Room.objects.get(sinav_yerleri[yer]).capacity
+            assert sinav_yerleri[yer] <= Room.objects.get(yer).capacity
