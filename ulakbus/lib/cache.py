3e3c1fbca1c204b9a9491d02dfb76f6cdb2c9175
furkanUyar <furkanuyar92@gmail.com>
2016-11-21 17:24:28 +0200

    ADD, Güncel dönemi cacheden almak için güncel dönem methodu genişletildi. rref #5529

--- ulakbus/lib/cache.py
+++ ulakbus/lib/cache.py
@@ -15,7 +15,39 @@ class RolePermissionCache(Cache):
     def __init__(self, role_id):
         super(RolePermissionCache, self).__init__(role_id)
 
-#
+
+class GuncelDonem(Cache):
+    """
+    Güncel dönemin tutulduğu cache objesi.
+    Cache'de güncel dönem datası varsa datalardan dönem objesi döndürür.
+    Yoksa veritabanından güncel dönemi döndürür ve cache'e 1 saatlik güncel dönem datasını koyar.
+
+    """
+
+    PREFIX = 'GUNDON'
+
+    def __init__(self, guncel_donem):
+        super(GuncelDonem, self).__init__(guncel_donem)
+
+    def get_or_create(self):
+
+        from ulakbus.models.ogrenci import Donem
+
+        if self.get():
+            return Donem(**self.get())
+        else:
+            guncel_donem = Donem.objects.get(guncel=True)
+            donem_fields = {'ad': guncel_donem.ad,
+                            'baslangic_tarihi': guncel_donem.baslangic_tarihi.strftime(
+                                "%d.%m.%Y"),
+                            'bitis_tarihi': guncel_donem.baslangic_tarihi.strftime(
+                                "%d.%m.%Y"),
+                            'guncel': guncel_donem.guncel,
+                            'key': guncel_donem.key}
+
+            self.set(donem_fields, 3600)
+            return guncel_donem
+
 # class Cache(Cache):
 #     PREFIX = ''
 #
