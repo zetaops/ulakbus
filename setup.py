59995996c33281e43ce2b4a6f29340b6d7a07282
Ali Riza Keles <ali.r.keles@gmail.com>
2016-12-23 11:26:44 +0300

    ADD: increase zengine and pyoko versions for dist

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='GPL v3',
     author_email='info@zetaops.io',
     install_requires=[
-        'zengine==0.7.6',
+        'zengine==0.7.7',
         'requests',
         'boto',
         'reportlab',
