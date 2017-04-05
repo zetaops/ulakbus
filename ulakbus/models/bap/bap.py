5a0e96731676b6e377c36ac9d65c67f1c12cb47a
Tolga Ozturk <oztrk.tolga@gmail.com>
2017-04-05 12:14:20 +0300

    ADD, bap takvim modeli olusturuldu. rref #5642

--- ulakbus/models/bap/bap.py
+++ ulakbus/models/bap/bap.py
@@ -3,10 +3,11 @@
 #
 # This file is licensed under the GNU General Public License v3
 # (GPLv3).  See LICENSE.txt for details.
+from ulakbus.models.form import Form
+
+from zengine.lib.translation import gettext_lazy as __
 
 from pyoko import Model, field, ListNode
-from zengine.lib.translation import gettext_lazy as __
-from ulakbus.models.form import Form
 
 
 class BAPProjeTurleri(Model):
@@ -18,10 +19,10 @@ class BAPProjeTurleri(Model):
     butce_ust_limit = field.Float(__(u"Projenin üst limiti"))
 
     class Meta:
-        app = 'Proje Türleri'
         verbose_name = __(u"Proje Türü")
         verbose_name_plural = __(u"Proje Türleri")
         list_fields = ['kod', 'ad', 'min_sure', 'max_sure', 'butce_ust_limit']
+        list_filters = ['kod', 'ad']
         search_fields = ['kod', 'ad', 'min_sure', 'max_sure', 'butce_ust_limit']
 
     class Belgeler(ListNode):
@@ -42,3 +43,47 @@ class BAPProjeTurleri(Model):
 
     def __unicode__(self):
         return "%s: %s" % (self.kod, self.ad)
+
+
+class BAPTakvim(Model):
+    class Meta:
+        search_fields = ['ProjeTuru', 'aciklama', ]
+        list_fields = ['donem', '_takvim_turu', '_tarih', 'takvim_aciklama']
+
+    class ProjeTuru(ListNode):
+        class Meta:
+            verbose_name = __(u"Proje Türü")
+            verbose_name_plural = __(u"Proje Türleri")
+
+        proje_turu = BAPProjeTurleri()
+
+    class OnemliTarihler(ListNode):
+        class Meta:
+            verbose_name = __(u"Önemli Tarih")
+            verbose_name_plural = __(u"Önemli Tarihler")
+
+        baslangic_tarihi = field.Date(__(u"Başlangıç Tarihi"))
+        bitis_tarihi = field.Date(__(u"Bitiş Tarihi"))
+        aciklama = field.Integer(__(u"Açıklama Seçiniz"), choices='onemli_tarih_aciklama')
+
+    takvim_aciklama = field.Text(__(u"Takvim Açıklaması"))
+    donem = field.Integer(__(u"Takvimin Yayınlanacağı Dönem"))
+
+    def __unicode__(self):
+        return "%s. Dönem: %s" % (self.donem, self.takvim_aciklama)
+
+    def _takvim_turu(self):
+        if self.ProjeTuru:
+            return ''.join(["""
+            %s\n""" % proje.proje_turu.ad for proje in self.ProjeTuru])
+        else:
+            return """
+            Genel Takvim"""
+
+    def _tarih(self):
+        return ''.join(["""
+        %s - %s\n""" % (tarih.baslangic_tarihi, tarih.bitis_tarihi) for tarih in
+                        self.OnemliTarihler])
+
+    _takvim_turu.title = __(u"Takvim Türü")
+    _tarih.title = __(u"Tarih")
