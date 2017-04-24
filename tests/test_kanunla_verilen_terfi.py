20f8192ca00b058c5ba82881e0d560cb27e02bf0
Mithat Raşit Özçıkrıkcı <mroz@konya.edu.tr>
2017-04-24 14:19:47 +0300

    Kanunla verilen terfi wf test yazıldı

--- tests/test_kanunla_verilen_terfi.py
+++ tests/test_kanunla_verilen_terfi.py
@@ -8,20 +8,20 @@
 
 from zengine.lib.test_utils import BaseTestCase
 from zengine.messaging.model import Message
-from ulakbus.models import User, Personel
+from ulakbus.models import User, Personel, HitapSebep
 import time
 import random
 
 class TestCase(BaseTestCase):
 
-    def terfi_test(self):
+    def test_terfi(self):
         """
             Kanunla verilen terfi workflow'a ait testleri içerir
         Returns:
 
         """
 
-        def kademe_derece_duzenle():
+        def kademe_derece_duzenle(terfi_form):
             """
                 Parametre olarak girilen kademe değerine göre personelin
                 derecesinin ne olacağını return eder.
@@ -49,8 +49,10 @@ class TestCase(BaseTestCase):
 
         usr = User.objects.get(username='mithat')
         personel = Personel.objects.filter()[random.randint(0,3000)]
+        personel_id = personel.key
         self.prepare_client('kanunla_verilen_terfi', user=usr)
-        self.client.post()
+        self.client.post(cmd="terfi_form", id=personel.key)
+        mesaj_sayisi = Message.objects.count()
 
         terfi_form = {
             "ga_kademe" : personel.gorev_ayligi_kademe,
@@ -61,8 +63,51 @@ class TestCase(BaseTestCase):
             "em_derece" : personel.emekli_muktesebat_derece
         }
 
-        kademe_derece_duzenle()
+        kademe_derece_duzenle(terfi_form)
 
-        resp = self.client.post(cmd="terfi_kaydet", form=terfi_form)
+        self.client.post(cmd="terfi_bilgileri_kaydet", form=terfi_form)
 
-        assert resp.json["msgbox"]["title"]
+        hizmet_cetveli_form = {
+            "terfi_sebep_id" : HitapSebep.objects.get(sebep_no=6),
+            "baslama_tarihi" : "10.04.2017",
+            "bitis_tarihi" : "25.04.2017",
+            "kurum_onay_tarihi" : "09.04.2017"
+        }
+
+        resp = self.client.post(cmd="hizmet_cetveli_kayit")
+
+        assert resp.json["msgbox"]["title"] == u'Onaya Gönderildi'
+
+        assert Message.objects.count() == mesaj_sayisi +1
+
+        mesaj_sayisi += 1
+
+        # Terfisi yapılan personel eğer idari personel ise onay makamı genel sekreterliktir.
+        # Terfisi yapılan personel eğer akademik personel ise onay makamı rektörlüktür.
+
+        if personel.personel_turu == 1:
+            usr = User.objects.get(username='rektor')
+        else:
+            usr = User.objects.get(username='genel_sekreter')
+
+        self.prepare_client('kanunla_verilen_terfi', user=usr)
+
+        resp = self.client.post(cmd='terfi_kaydet')
+
+        assert resp.json["msgbox"]["title"] == u"Terfi İşlemi Gerçekleştirildi"
+
+        assert Message.objects.count() == mesaj_sayisi + 2
+
+        personel = Personel.objects.get(personel_id)
+
+        assert personel.gorev_ayligi_kademe == terfi_form["ga_kademe"]
+
+        assert personel.gorev_ayligi_derece == terfi_form["ga_derece"]
+
+        assert personel.kazanilmis_hak_kademe == terfi_form["kh_kademe"]
+
+        assert personel.kazanilmis_hak_derece == terfi_form["kh_derece"]
+
+        assert personel.emekli_muktesebat_kademe == terfi_form["em_kademe"]
+
+        assert personel.emekli_muktesebat_derece == terfi_form["em_derece"]
