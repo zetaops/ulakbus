2b2470a23a9fca9b7de741c74e460becef412f83
Ali Riza Keles <ali.r.keles@gmail.com>
2017-05-23 15:26:57 +0300

    FIX form date alani icin varsayilan deger formati degistirildi.

--- ulakbus/views/personel/kadro.py
+++ ulakbus/views/personel/kadro.py
@@ -69,6 +69,7 @@ from ulakbus.models import Personel
 from pyoko import ListNode
 from dateutil.relativedelta import relativedelta
 import datetime
+from pyoko.fields import DATE_TIME_FORMAT
 
 
 class KadroObjectForm(JsonForm):
@@ -407,10 +408,10 @@ class TerfiDuzenleForm(JsonForm):
 
 class PersonelTerfiKriterleri(JsonForm):
     baslangic_tarihi = fields.Date(gettext_lazy(u"Başlangıç Tarihi"),
-                                   default=datetime.date.today().strftime('%d.%m.%Y'))
+                                   default=datetime.date.today().strftime(DATE_TIME_FORMAT))
 
     bitis_tarihi = fields.Date(gettext_lazy(u"Bitiş Tarihi"), default=(
-        datetime.date.today() + datetime.timedelta(days=15)).strftime('%d.%m.%Y'))
+        datetime.date.today() + datetime.timedelta(days=15)).strftime(DATE_TIME_FORMAT))
 
     personel_turu = fields.Integer(gettext_lazy(u"Personel Türü"),
                                    choices=[(1, gettext_lazy(u"Akademik")),
