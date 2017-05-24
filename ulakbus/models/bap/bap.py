073321c6fc76edd34fe380458f414a608babcdf7
furkanuyar <furkanuyar92@gmail.com>
2017-05-24 16:39:29 +0300

    FIX, proje field'ı ilgili_proje olarak değiştirildi.

--- ulakbus/models/bap/bap.py
+++ ulakbus/models/bap/bap.py
@@ -250,8 +250,7 @@ class BAPButcePlani(Model):
     class Meta:
         verbose_name = __(u"Bap Bütçe Planı")
         verbose_name_plural = __(u"Bap Bütçe Planları")
-        list_fields = ['_muhasebe_kod', 'kod_adi', 'ad', 'birim_fiyat', 'adet',
-                       'toplam_fiyat']
+        list_fields = ['_muhasebe_kod', 'kod_adi', 'ad', 'birim_fiyat', 'adet', 'toplam_fiyat']
 
     muhasebe_kod = field.String(__(u"Muhasebe Kod"),
                                 choices='analitik_butce_dorduncu_duzey_gider_kodlari',
