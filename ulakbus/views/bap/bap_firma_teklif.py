d1454ace73fcb2d3afba38eb95277ec3892e8f11
furkanuyar <furkanuyar92@gmail.com>
2017-05-24 17:33:26 +0300

    ADD, firma teklif iş akışına dokümantasyon eklendi. rref #5686

--- ulakbus/views/bap/bap_firma_teklif.py
+++ ulakbus/views/bap/bap_firma_teklif.py
@@ -11,6 +11,11 @@ from ulakbus.lib.common import get_file_url
 
 
 class FirmaTeklifForm(JsonForm):
+    """
+    Teklifin yapıldığı ve düzenlendiği form.
+    
+    """
+
     class Meta:
         include = ['Belgeler']
 
@@ -19,6 +24,11 @@ class FirmaTeklifForm(JsonForm):
 
 
 class BapFirmaTeklif(CrudView):
+    """
+    Firmaların, teklife açık bütçe kalemlerine teklif vermesini sağlayan iş akışı.
+    
+    """
+
     class Meta:
         model = 'BAPButcePlani'
 
@@ -30,10 +40,20 @@ class BapFirmaTeklif(CrudView):
         # self.firma = self.current.user.bap_firma_set[0].bap_firma
 
     def ayni_butce_kalemi_teklif_kontrol(self):
+        """
+        Bir firma tarafından, bir bütçe kalemine teklif yapılmışsa, 
+        aynı bütçe kalemine yeni bir teklif yapılmasını kontrol eder.
+                
+        """
         teklif_sayisi = BAPTeklif.objects.filter(butce=self.object, firma=self.firma).count()
         self.current.task_data['ayni_teklif'] = True if teklif_sayisi else False
 
     def ayni_teklif_var_mesaji(self):
+        """
+        Bir bütçe kalemine, firma tarafından yapılmış teklif varken, yeni 
+        bir teklif yapılmaya çalışıldığında uyarı mesajı gösterilir. 
+
+        """
         self.current.output['msgbox'] = {"type": "warning",
                                          "title": __(u'Mevcut Teklif Uyarısı'),
                                          "msg": __(u"%s adlı bütçe kalemine ait teklifiniz "
@@ -42,15 +62,28 @@ class BapFirmaTeklif(CrudView):
                                                    u"yapabilirsiniz." % self.object.ad)}
 
     def mevcut_teklifler_kontrol(self):
+        """
+        Değerlendirme halinde olan mevcut herhangi bir teklifin olup olmadığını kontrol eder. 
+
+        """
         teklif_sayisi = BAPTeklif.objects.filter(firma=self.firma, durum=1).count()
         self.current.task_data['mevcut_teklifler'] = True if teklif_sayisi else False
 
     def mevcut_teklif_yok_mesaji(self):
+        """
+        Mevcut teklif yokken, mevcut tekliflerin görüntülenmesi 
+        istenirse uyarı mesajı üretilir ve gösterilir.
+
+        """
         self.current.output['msgbox'] = {"type": "warning",
                                          "title": __(u'Firma Teklifleri'),
                                          "msg": __(u"Sistemde kayıtlı teklifiniz bulunmamaktadır.")}
 
     def mevcut_teklifleri_goster(self):
+        """
+        Firmaya ait değerlendirme halinde olan güncel teklifler gösterilir.
+
+        """
         form = JsonForm(title=_(u'Firmanın Mevcut Teklifleri'))
         form.tamam = fields.Button(__(u"Geri Dön"), cmd='tamam')
         teklifler = BAPTeklif.objects.filter(firma=self.firma, durum=1).order_by()
@@ -71,53 +104,33 @@ class BapFirmaTeklif(CrudView):
         self.form_out(form)
 
     def teklif_belgeleri_indir(self):
+        """
+        Seçilen teklife ait teklif belgeleri indirilir.
+                
+        """
         teklif = BAPTeklif.objects.get(self.current.input['data_key'])
         belge_urls = [get_file_url(belge.belge) for belge in teklif.Belgeler]
         self.set_client_cmd('download')
         self.current.output['download_url'] = belge_urls[0]
 
     def teklif_belgeleri_duzenle(self):
+        """
+        Seçilen teklife ait teklif belgeleri düzenlenir. Mevcut 
+        belgeler silinebilir, yeni belgeler yüklenebilir. 
+
+        """
         self.current.task_data['new'] = False
         self.current.task_data['data_key'] = self.current.input['data_key']
         teklif = BAPTeklif.objects.get(self.current.input['data_key'])
         self.form_out(
             FirmaTeklifForm(teklif, current=self.current, title=__(u"Teklif Belgeleri Düzenleme")))
 
-    def islem_sonrasi_mesaj(self):
-        self.current.output['msgbox'] = {"type": "info",
-                                         "title": __(u'Firma Teklifleri'),
-                                         "msg": __(u"Teklif belgeleriniz sisteme başarıyla"
-                                                   u" yüklenmiştir. Mevcut Tekliflerim altından"
-                                                   u" teklif belgelerinize ulaşabilir ve "
-                                                   u"belgelerinizde değişiklikler yapabilirsiniz.")}
-
-    def teklif_ver(self):
-        self.current.task_data['new'] = True
-        form = FirmaTeklifForm(BAPTeklif(), current=self.current, title=__(u"Bütçe Kalemi Teklifi"))
-        form.help_text = __(
-            u"Lütfen %s adlı bütçe kalemine yaptığınız teklife "
-            u"ilişkin belgeleri sisteme yükleyiniz.") % self.object.ad
-        self.form_out(form)
-
-    def teklif_belge_kontrol(self):
-        self.current.task_data['belge'] = True if len(self.input['form']['Belgeler']) else False
-
-    def belge_eksikligi_mesaj(self):
-        self.current.output['msgbox'] = {"type": "warning",
-                                         "title": __(u'Belge Eksikliği'),
-                                         "msg": __(u"Teklifinize ilişkin belge ya da belgeleri"
-                                                   u" yüklemeniz gerekmektedir. Lütfen kontrol edip"
-                                                   u" tekrar deneyiniz. ")}
-
-    def teklif_kaydet(self):
-        teklif = BAPTeklif(butce=self.object, firma=self.firma)
-        form_belgeler = [(belge['belge'], belge['aciklama']) for belge in
-                         self.input['form']['Belgeler']]
-        [teklif.Belgeler(belge=belge, aciklama=aciklama) for belge, aciklama in form_belgeler]
-        teklif.durum = 1
-        teklif.blocking_save()
-
     def teklif_duzenle_kaydet(self):
+        """
+        Teklif belgeleri düzenlendikten sonra Belgeler kısmı formdan 
+        gelen belgelere göre tekrardan düzenlenir ve kaydedilir.        
+
+        """
         teklif = BAPTeklif.objects.get(self.current.task_data['data_key'])
         form_belgeler = [belge['belge'] for belge in self.input['form']['Belgeler']]
 
@@ -130,6 +143,62 @@ class BapFirmaTeklif(CrudView):
         [teklif.Belgeler(belge=belge) for belge in yeni]
         teklif.blocking_save()
 
+    def islem_sonrasi_mesaj(self):
+        """
+        Teklif yeni kaydedildikten sonra ve düzenlemeden 
+        sonra işlem mesajı üretilir ve gösterilir.
+         
+        """
+        self.current.output['msgbox'] = {"type": "info",
+                                         "title": __(u'Firma Teklifleri'),
+                                         "msg": __(u"Teklif belgeleriniz sisteme başarıyla"
+                                                   u" yüklenmiştir. Mevcut Tekliflerim altından"
+                                                   u" teklif belgelerinize ulaşabilir ve "
+                                                   u"belgelerinizde değişiklikler yapabilirsiniz.")}
+
+    def teklif_ver(self):
+        """
+        Teklifin yapıldığı, belgelerin eklenebildiği ekran oluşturulur.         
+
+        """
+        self.current.task_data['new'] = True
+        form = FirmaTeklifForm(BAPTeklif(), current=self.current, title=__(u"Bütçe Kalemi Teklifi"))
+        form.help_text = __(
+            u"Lütfen %s adlı bütçe kalemine yaptığınız teklife "
+            u"ilişkin belgeleri sisteme yükleyiniz.") % self.object.ad
+        self.form_out(form)
+
+    def teklif_belge_kontrol(self):
+        """
+        Teklife ilişkin herhangi bir belgenin eklenip eklenmediği kontrol edilir.        
+
+        """
+        self.current.task_data['belge'] = True if len(self.input['form']['Belgeler']) else False
+
+    def belge_eksikligi_mesaj(self):
+        """
+        Teklife ilişkin bir belge eklenmemişse uyarı mesajı üretilir ve gösterilir.      
+
+        """
+        self.current.output['msgbox'] = {"type": "warning",
+                                         "title": __(u'Belge Eksikliği'),
+                                         "msg": __(u"Teklifinize ilişkin belge ya da belgeleri"
+                                                   u" yüklemeniz gerekmektedir. Lütfen kontrol edip"
+                                                   u" tekrar deneyiniz. ")}
+
+    def teklif_kaydet(self):
+        """
+        Teklif veritabanına kaydedilir ve durumu değerlendirme 
+        sürecinde anlamına gelen 1 yapılır.
+
+        """
+        teklif = BAPTeklif(butce=self.object, firma=self.firma)
+        form_belgeler = [(belge['belge'], belge['aciklama']) for belge in
+                         self.input['form']['Belgeler']]
+        [teklif.Belgeler(belge=belge, aciklama=aciklama) for belge, aciklama in form_belgeler]
+        teklif.durum = 1
+        teklif.blocking_save()
+
     @obj_filter
     def firma_kayit_actions(self, obj, result):
         result['actions'] = [
@@ -138,5 +207,9 @@ class BapFirmaTeklif(CrudView):
         ]
 
     @list_query
-    def onaylanmis_butce_kalemleri_listele(self, queryset):
+    def teklife_acik_butce_kalemleri_listele(self, queryset):
+        """
+        Durumu 1 olan yani, teklife açık olan bütçe kalemleri listelenir.
+                
+        """
         return queryset.filter(teklif_durum=1)
