716271fd153ce2ae36c7c319652a7b9241cc4e90
Anil Can Aydin <anlcnydn@gmail.com>
2017-06-28 16:31:31 +0300

    ADD, Ogretim uyesi basvuru listeleme is akis diagrami cizildi. rref #5655

---
+++ ulakbus/views/bap/bap_ogretim_uyesi_basvuru_listeleme.py
@@ -0,0 +1,13 @@
+# -*-  coding: utf-8 -*-
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+
+from zengine.forms import JsonForm, fields
+from zengine.views.crud import CrudView
+from zengine.lib.translation import gettext as _, gettext_lazy as __
+
+
+class OgretimUyesiBasvuruListelemeView(CrudView):
+    pass
