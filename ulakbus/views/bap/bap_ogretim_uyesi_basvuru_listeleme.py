de0293c570aa2f816e0a41b994d261da92f42273
Anil Can Aydin <anlcnydn@gmail.com>
2017-06-29 16:11:21 +0300

    ADD, Ogretim uyesi basvuru listeleme is akis diagramina talep secme ve rapor secme adimlari eklendi. Listeleme adimi tamamlandi. rref #5655

--- ulakbus/views/bap/bap_ogretim_uyesi_basvuru_listeleme.py
+++ ulakbus/views/bap/bap_ogretim_uyesi_basvuru_listeleme.py
@@ -3,11 +3,53 @@
 #
 # This file is licensed under the GNU General Public License v3
 # (GPLv3).  See LICENSE.txt for details.
-
+from ulakbus.models import BAPProje
 from zengine.forms import JsonForm, fields
-from zengine.views.crud import CrudView
-from zengine.lib.translation import gettext as _, gettext_lazy as __
+from zengine.views.crud import CrudView, list_query, obj_filter
+from zengine.lib.translation import gettext as _
 
 
 class OgretimUyesiBasvuruListelemeView(CrudView):
-    pass
+
+    class Meta:
+        allow_search = True
+        model = 'BAPProje'
+
+    def __init__(self, current=None):
+        CrudView.__init__(self, current)
+        self.ListForm.add = None
+
+    def basvuru_listele(self):
+        self.list(list_fields=['ad', 'durum'])
+
+    def talep_sec(self):
+        pass
+
+    def rapor_sec(self):
+        pass
+
+    @obj_filter
+    def basvuru_islemleri(self, obj, result, **kwargs):
+        """
+        Default action buttonlar, öğretim üyesinin projedeki eylemlerine göre düzenlenmiştir.
+
+        """
+        # todo externaL_wf'ler tamamlandıkça actionlara eklenecek
+        result['actions'] = [
+            {'name': _(u'Görüntüle'), 'cmd': 'goruntule', 'mode': 'normal', 'show_as': 'button'},
+        ]
+        if obj.durum == 5:
+            result['actions'].append({'name': _(u'Talepler'), 'cmd': 'talepler', 'mode': 'normal',
+                                      'show_as': 'button'})
+            result['actions'].append({'name': _(u'Raporlar'), 'cmd': 'rapor', 'mode': 'normal',
+                                      'show_as': 'button'})
+
+    @list_query
+    def list_by_personel_id(self, queryset):
+        """
+        Öğretim üyesinin kendi projeleri filtrelenmiştir.
+        """
+        # return queryset.filter(yurutucu_id=self.current.user_id)
+        return queryset.filter(yurutucu_id='G2XjlaJMX0FUZX84aoIeiVCqZMR')
+
+
