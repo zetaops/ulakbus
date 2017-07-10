7c4b5532996ba82d33eaca841441112cafb107f9
xsetra <abdullahcaliskan511@gmail.com>
2017-07-10 17:10:52 +0300

    ADD, RenderDocuments class added for zato services

--- ulakbus/services/common/document_render.py
+++ ulakbus/services/common/document_render.py
@@ -33,6 +33,10 @@ class RenderDocument(Service):
         renderer = self.outgoing.plain_http.get('DocumentRenderer')
 
         headers = {'X-App-Name': 'Zato', 'Content-Type': 'application/json'}
+
         resp = renderer.conn.send(self.cid, data=self.request.payload, headers=headers)
+
+        self.logger.info('Response: {}'.format(resp))
+        self.logger.info('Response-Type: {}'.format(type(resp)))
 
         self.response.payload = resp.data
