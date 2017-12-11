a3bf107c7373bf5ae285e46cbf5aa7f3ccc62e8c
Anil Can Aydin <anlcnydn@gmail.com>
2017-12-11 12:11:07 +0300

    Bap Satın Alma Duyuru Listeleme is akisi review edildi

--- ulakbus/views/bap/bap_satin_alma_duyurulari_listeleme.py
+++ ulakbus/views/bap/bap_satin_alma_duyurulari_listeleme.py
@@ -38,45 +38,17 @@ class BAPSatinAlmaDuyurulariListeleme(CrudView):
         ]
         form = JsonForm(title=__(u"Satın Alma Duyurularının Listesi"))
         objs = BAPSatinAlma.objects.filter(teklife_kapanma_tarihi__gt=datetime.today(),
-               duyuruda=True).order_by('teklife_acilma_tarihi')
+                                           duyuruda=True).order_by('teklife_acilma_tarihi')
         for o in objs:
             self.output['objects'].append({
-                "fields": [o.ad, "%s %s" % (o.sorumlu.user.personel.ad, o.sorumlu.user.personel.soyad),
+                "fields": [o.ad,
+                           "%s %s" % (o.sorumlu.user.personel.ad, o.sorumlu.user.personel.soyad),
                            o.teklife_kapanma_tarihi.strftime("%d.%m.%Y")],
                 "actions": [{'name': _(u'Detay'), 'cmd': 'detay', 'show_as': 'button'}],
                 "key": o.key
-                })
+            })
         self.form_out(form)
 
-            # ad = field.String(__(u"Satın Alma Duyuru Adı"))
-            # teklife_acilma_tarihi = field.DateTime(__(u"Teklife Açılma Tarihi"))
-            # teklife_kapanma_tarihi = field.DateTime(__(u"Teklife Kapanma Tarihi"))
-            # sonuclanma_tarihi = field.Date(__(u"Teklifin Sonuçlanma Tarihi"))
-            # onay_tarih_sayi = field.String(__(u"Onay Tarih/Sayı"))
-            # ekleyen = Personel()
-            # aciklama = field.Text(__(u"Açıklama"))
-            # teklif_durum = field.Integer(__(u"Teklif Durum"), choices='bap_satin_alma_durum')
-            # ilgili_proje = BAPProje()
-            # tek_firma = BAPFirma()
-            # tur = field.Integer(_(u"Satın Alma Türü"), choices='bap_satin_alma_turleri')
-            # sorumlu = Role()
-            # duyuruda = field.Boolean(__(u"Duyurulma Durumu"), default=False)
-
-
-
-        """
-        1. Alternatif teklifler değerlendirmeye alınmayacaktır.
-        2. Marka model ve/veya katalog numarası belirtilmeyen teklifler değerlendirme dışı bırakılacaktır.
-        3. Satın alınacak mal/malzemenin kalem sırasında ve birim miktarlarında değişiklik yapılmaması esastır. Tutarsızlık halinde teklif geçersiz sayılacaktır.
-        4. Teklif numarası ve proje numarası belirtilmeyen teklifler değerlendirmeye alınmayacaktır.
-        5. Her bir kalem için KDV oranı belirtilmelidir.
-        6. Teklifte tarih, imza ve kaşe eksikliği olmamalıdır.
-        7. İstenilen bilgiler tam ve eksiksiz olmalıdır.
-        8. Kitlerde kullanım süresi (miyadı) tercih sebebidir.
-        9. Teknik şartnameye uygun teklif verilmelidir. Teknik şartnamenin kabul edildiği ve şartnameye uygun teklif verildiği verildiği teklif mektubunda belirtilmelidir. Şartnamede belirtilen ürün teslim edilmelidir.
-        10. Teslimat sipariş formunda belirtilen adrese yapılmalı ve sipariş formu fatura ekinde olmalıdır.
-        
-        """
     def duyuru_detay_goruntule(self):
         """
         Seçili olan satın alma duyurusuna ait bütçe kalemi bilgileri listelenir. Şartname dosyası
